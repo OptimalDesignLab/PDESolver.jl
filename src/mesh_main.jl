b02dac6542eae878993cab5c94530c2dcda89cfb
Jared Crean <jcrean01@gmail.com>
2015-02-03 17:49:39 -0500

    Added mesh_main.jl for real

---
+++ src/mesh_main.jl
@@ -0,0 +1,14 @@
+# Main file contianing mesh related functions and types
+
+abstract Mesh  # declare an abstract type called Mesh
+
+# define concrete types here
+type SimmetrixMesh <: Mesh
+  # define some data  members here
+
+end
+
+type LocalMesh <: Mesh
+  # define some data members here
+
+end
