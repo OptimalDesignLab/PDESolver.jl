9d7a7c834e50c25957683e7d82dd66619b5add17
Jared Crean <jcrean01@gmail.com>
2015-08-04 14:02:13 -0400

    most of euler.jl is tested

--- test/test_lowlevel.jl
+++ test/test_lowlevel.jl
@@ -1,5 +1,6 @@
 include("../src/solver/euler/startup.jl")  # initialization and construction
-
+fill!(eqn.SL, 0.0)
+using ArrayViews
 facts("--- Testing Mesh --- ") do
 
   @fact mesh.numVert => 4
@@ -135,8 +136,122 @@ facts("--- Testing Euler Low Level Functions --- ") do
  end
 
 
+ context("--- Testing dataPrep ---") do
+ 
+   EulerEquationMod.dataPrep(mesh, sbp, eqn, eqn.SL0, opts)
+
+
+   # test disassembleSolutino
+   for i=1:mesh.numEl
+     for j=1:mesh.numNodesPerElement
+       @fact eqn.q[:, j, i] => roughly([1.0, 0.35355, 0.35355, 2.0], atol=1e-5)
+     end
+   end
+
+   #=
+   for i=1:mesh.numEl
+     println("i = ", i)
+     for j=1:mesh.numNodesPerElement
+       println("j = ", j)
+       aux_vars_i = eqn.aux_vars[ :, j, i]
+       println("aux_vars_i = ", aux_vars_i)
+       p = EulerEquationMod.@getPressure(aux_vars_i)
+       @fact p => roughly(0.750001, atol=1e-5)
+     end
+   end
+   =#
+
+   # test calcEulerFlux
+   for i=1:mesh.numNodesPerElement
+     println("eq.F_xi[:, $i, 1, 1] = ", eqn.F_xi[:, i, 1, 1])
+     @fact eqn.F_xi[:, i, 1, 1] => roughly([0.0, 0.750001, -0.750001, 0.0], atol=1e-5)
+   end
+
+   for i=1:mesh.numNodesPerElement
+     @fact eqn.F_xi[:, i, 2, 1] => roughly([0.35355, 0.874999, 0.12499, 0.972263], atol=1e-5)
+   end
+
+   for i=1:mesh.numNodesPerElement
+     @fact eqn.F_xi[:, i, 1, 2] => roughly([0.35355, 0.124998, 0.874999, 0.972263], atol=1e-5)
+   end
+
+   for i=1:mesh.numNodesPerElement
+     @fact eqn.F_xi[:, i, 2, 2] => roughly([0.0, -0.750001, 0.750001, 0.0], atol=1e-5)
+   end
+
+
+   # test getBCFluxes
+     for j= 1:sbp.numfacenodes
+       @fact eqn.bndryflux[:, j, 1] => roughly([-0.35355, -0.874999, -0.124998, -0.972263], atol=1e-5)
+     end
+
+     for j= 1:sbp.numfacenodes
+       @fact eqn.bndryflux[:, j, 2] => roughly([-0.35355,  -0.124998, -0.874999, -0.972263], atol=1e-5)
+     end
+
+     for j= 1:sbp.numfacenodes
+       @fact eqn.bndryflux[:, j, 3] => roughly([0.35355,  0.124998, 0.874999, 0.972263], atol=1e-5)
+     end
+
+     for j= 1:sbp.numfacenodes
+       @fact eqn.bndryflux[:, j, 4] => roughly([0.35355, 0.874999, 0.124998, 0.972263], atol=1e-5)
+     end
 
 
 
+  end
 
- end
+
+  context("--- Testing evalVolumeIntegrals ---")  do
+
+    EulerEquationMod.evalVolumeIntegrals(mesh, sbp, eqn)
+
+    el1_res = [-0.35355  0  0.35355;
+                -0.874999  0.750001  0.124998;
+		-0.124998  -0.750001  0.874999;
+		-0.972263  0  0.972263]
+    el2_res = [-0.35355  0.35355 0;
+                -0.124998  0.874999 -0.75001;
+		-0.874999 0.124998 0.75001;
+		-0.972263  0.972263 0]
+ 
+    @fact eqn.res[:, :, 1] => roughly(el1_res, atol=1e-5)
+    @fact eqn.res[:, :, 2] => roughly(el2_res, atol=1e-5)
+
+
+
+  end
+
+
+  context("--- Testing evalBoundaryIntegrals ---") do
+    fill!(eqn.res, 0.0)
+
+    EulerEquationMod.evalBoundaryIntegrals( mesh, sbp, eqn)
+
+    el1_res = [0.35355 0 -0.35355;
+               0.124998 -0.750001 -0.874999;
+	       0.874999 0.750001 -0.124998;
+	       0.972263  0  -0.972263]
+    el2_res = [0.35355 -0.35355 0;
+               0.874999 -0.124998 0.750001;
+	       0.124998  -0.874999  -0.750001;
+	       0.972263  -0.972263  0]
+
+    @fact eqn.res[:, :, 1] => roughly(el1_res, atol=1e-5)
+    @fact eqn.res[:, :, 2] => roughly(el2_res, atol=1e-5)
+
+  end
+
+  context("--- Testing evalEuler --- ")  do
+
+    fill!(eqn.SL, 0.0)
+    fill!(eqn.res, 0.0)
+    EulerEquationMod.evalEuler(mesh, sbp, eqn, opts, eqn.SL0, eqn.SL)
+
+    for i=1:mesh.numDof
+      eqn.SL[i] => roughly(0.0, atol=1e-14)
+    end
+
+  end
+
+end # end facts block
