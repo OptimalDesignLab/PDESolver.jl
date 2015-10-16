aa535bf60fc7e65c8347d8810e77b91461dc1d99
Anthony Ashley <ashlea@rpi.edu>
2015-10-16 14:49:58 -0400

    code cleanup done on EulerEquationMod

--- src/solver/euler/EulerEquationMod.jl
+++ src/solver/euler/EulerEquationMod.jl
@@ -225,24 +225,26 @@ type EulerData_{Tsol, Tres, Tdim, Tmsh} <: EulerData{Tsol, Tdim}
   q::Array{Tsol,3}  # holds conservative variables for all nodes
   # hold fluxes in all directions
   # [ndof per node by nnodes per element by num element by num dimensions]
-  aux_vars::Array{Tres, 3}  # storage for auxiliary variables 
+  aux_vars::Array{Tres, 3}        # storage for auxiliary variables 
   flux_parametric::Array{Tsol,4}  # flux in xi and eta direction
-  res::Array{Tres, 3}  # result of computation
-  SL::Array{Tres, 1}  # result of computation in vector form
-  SL0::Array{Tres,1}  # initial condition in vector form
+  res::Array{Tres, 3}             # result of computation
+  SL::Array{Tres, 1}              # result of computation in vector form
+  SL0::Array{Tres,1}              # initial condition in vector form
 
-  edgestab_alpha::Array{Tmsh, 4} # alpha needed by edgestabilization
-  bndryflux::Array{Tsol, 3}  # boundary flux
-  stabscale::Array{Tsol, 2}  # stabilization scale factor
+  edgestab_alpha::Array{Tmsh, 4}  # alpha needed by edgestabilization
+  bndryflux::Array{Tsol, 3}       # boundary flux
+  stabscale::Array{Tsol, 2}       # stabilization scale factor
 
-  dissipation_mat::Array{Tmsh, 3}  # artificial dissipation operator, a square numnodes x numnodes matrix for every element
-  Minv::Array{Float64, 1}  # invese mass matrix
-  M::Array{Float64, 1}  # mass matrix
-  disassembleSolution::Function # function SL0 -> eqn.q
-  assembleSolution::Function  # function : eqn.res -> SL
+  # artificial dissipation operator:
+  #   a square numnodes x numnodes matrix for every element
+  dissipation_mat::Array{Tmsh, 3}  
+
+  Minv::Array{Float64, 1}         # inverse mass matrix
+  M::Array{Float64, 1}            # mass matrix
+  disassembleSolution::Function   # function SL0 -> eqn.q
+  assembleSolution::Function      # function : eqn.res -> SL
 
   # inner constructor
-#  function EulerData(mesh::PumiMesh2, sbp::SBPOperator, T2::DataType)
   function EulerData_(mesh::PumiMesh2, sbp::SBPOperator, opts)
 
     println("\nConstruction EulerData object")
@@ -250,15 +252,9 @@ type EulerData_{Tsol, Tres, Tdim, Tmsh} <: EulerData{Tsol, Tdim}
     println("  Tres = ", Tres)
     println("  Tdim = ", Tdim)
     println("  Tmsh = ", Tmsh)
-    eqn = new()  # incomplete initilization
+    eqn = new()  # incomplete initialization
 
     eqn.params = ParamType{Tdim}(sbp, opts, mesh.order)
-#=
-    eqn.gamma = 1.4
-    eqn.gamma_1 = eqn.gamma - 1
-    eqn.R = 287.058  # specific gas constant (unit J/(kg * K)
-    eqn.cv = eqn.R/(eqn.gamma - 1)
-=#
     eqn.res_type = Tres
     eqn.disassembleSolution = disassembleSolution
     eqn.assembleSolution = assembleSolution
@@ -270,42 +266,34 @@ type EulerData_{Tsol, Tres, Tdim, Tmsh} <: EulerData{Tsol, Tdim}
 
     if opts["use_dissipation"]
       dissipation_name = opts["dissipation_name"]
-      eqn.dissipation_mat = calcDissipationOperator(mesh, sbp, eqn, dissipation_name, opts)
+      eqn.dissipation_mat = calcDissipationOperator(mesh, sbp, eqn, 
+                                                    dissipation_name, opts)
     else
       eqn.dissipation_mat = Array(Tmsh, 0, 0, 0)
     end
     
-    # must initialize them because some datatypes (BigFloat) 
-    # don't automatically initializes them
-    # taking a view(A,...) of undefined values is illegal
-    # I think its a bug that Array(Float64, ...) initailizes values
+    # Must initialize them because some datatypes (BigFloat) 
+    #   don't automatically initialize them
+    # Taking a view(A,...) of undefined values is illegal
+    # I think its a bug that Array(Float64, ...) initializes values
     eqn.q = zeros(Tsol, mesh.numDofPerNode, sbp.numnodes, mesh.numEl)
     eqn.aux_vars = zeros(Tsol, 1, sbp.numnodes, mesh.numEl)
-    eqn.flux_parametric = zeros(Tsol, mesh.numDofPerNode, sbp.numnodes, mesh.numEl, Tdim)
-#    eqn.F_eta = Array(Tsol, mesh.numDofPerNode, sbp.numnodes, mesh.numEl)
-  #  eqn.res = Array(T2, mesh.numDofPerNode, sbp.numnodes, mesh.numEl)
+    eqn.flux_parametric = zeros(Tsol, mesh.numDofPerNode, sbp.numnodes, 
+                                mesh.numEl, Tdim)
     eqn.res = zeros(Tres, mesh.numDofPerNode, sbp.numnodes, mesh.numEl)
     eqn.SL = zeros(Tres, mesh.numDof)
     eqn.SL0 = zeros(Tres, mesh.numDof)
-
-    eqn.bndryflux = zeros(Tsol, mesh.numDofPerNode, sbp.numfacenodes, mesh.numBoundaryEdges)
+    eqn.bndryflux = zeros(Tsol, mesh.numDofPerNode, sbp.numfacenodes, 
+                          mesh.numBoundaryEdges)
     eqn.stabscale = zeros(Tres, sbp.numnodes, mesh.numInterfaces)
 
-    #println("typeof(operator.Q[1]) = ", typeof(operator.Q[1]))
-    #type_of_sbp = typeof(operator.Q[1])  # a little hackish
-    #return EulerData(cv, R, gamma, bigQT_xi, bigQT_eta, Array(type_of_sbp,0,0,0), Array(type_of_sbp, 0,0,0), Array(type_of_sbp, 0,0,0), Array(type_of_sbp,0,0,0))
-    #return EulerData(cv, R, gamma, bigQT_xi, bigQT_eta)
-
     println("eqn.res_type = ", eqn.res_type)
+
     return eqn
+
   end  # end of constructor
 
 end  # end of type declaration
-
-
-
-
-
 
 @doc """
 ### EulerEquationMod.calcMassMatrixInverse
@@ -315,27 +303,24 @@ end  # end of type declaration
   in a vector.  mesh.dofs is used to put the components of the inverse
   mass matrix in the same place as the corresponding values in eqn.SL
 
-
 """->
 # used by EulerData Constructor
 # mid level functions
-function calcMassMatrixInverse{Tmsh,  Tsol, Tdim}(mesh::AbstractMesh{Tmsh}, sbp::SBPOperator, eqn::EulerData{Tsol, Tdim} )
+function calcMassMatrixInverse{Tmsh,  Tsol, Tdim}(mesh::AbstractMesh{Tmsh}, 
+                                                  sbp::SBPOperator, 
+                                                  eqn::EulerData{Tsol, Tdim})
 # calculate the inverse mass matrix so it can be applied to the entire solution vector
 # mass matrix is diagonal, stores in vector eqn.Minv
 
   eqn.Minv = zeros(Tmsh, mesh.numDof)
 
   for i=1:mesh.numEl
-#    dofnums_i =  getGlobalNodeNumbers(mesh, i)
     for j=1:sbp.numnodes
       for k=1:mesh.numDofPerNode
-#	dofnum_k = dofnums_i[k,j]
         dofnum_k = mesh.dofs[k,j,i]
-	# multiplication is faster than division, so do the divions here
-	# and then multiply solution vector times Minv
-	eqn.Minv[dofnum_k] += (sbp.w[j]/mesh.jac[j,i])
-
-#	eqn.Minv[dofnum_k] *= 1/(sbp.w[j])
+        # multiplication is faster than division, so do the divisions here
+        # and then multiply solution vector times Minv
+        eqn.Minv[dofnum_k] += (sbp.w[j]/mesh.jac[j,i])
       end
     end
   end
@@ -346,32 +331,29 @@ function calcMassMatrixInverse{Tmsh,  Tsol, Tdim}(mesh::AbstractMesh{Tmsh}, sbp:
 
   return nothing
 
-end
+end     # end of calcMassMatrixInverse function
 
-function calcMassMatrix{Tmsh,  Tsol, Tdim}(mesh::AbstractMesh{Tmsh}, sbp::SBPOperator, eqn::EulerData{Tsol, Tdim} )
+function calcMassMatrix{Tmsh,  Tsol, Tdim}(mesh::AbstractMesh{Tmsh}, 
+                                           sbp::SBPOperator, 
+                                           eqn::EulerData{Tsol, Tdim})
 # calculate the (diagonal) mass matrix as a vector
-# return tehe vector M
+# return the vector M
 
   M = zeros(Tmsh, mesh.numDof)
 
   for i=1:mesh.numEl
-#    dofnums_i =  getGlobalNodeNumbers(mesh, i)
     for j=1:sbp.numnodes
       for k=1:mesh.numDofPerNode
-#	dofnum_k = dofnums_i[k,j]
         dofnum_k = mesh.dofs[k,j,i]
-	# multiplication is faster than division, so do the divions here
-	# and then multiply solution vector times M
-	M[dofnum_k] += (sbp.w[j]/mesh.jac[j,i])
-
-#	eqn.M[dofnum_k] *= 1/(sbp.w[j])
+        # multiplication is faster than division, so do the divions here
+        # and then multiply solution vector times M
+        M[dofnum_k] += (sbp.w[j]/mesh.jac[j,i])
       end
     end
   end
 
   return M
 
-end
-
+end     # end of calcMassMatrix function
 
 end # end module
