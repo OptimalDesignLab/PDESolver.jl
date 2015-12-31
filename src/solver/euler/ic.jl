08154706b38e43754a8bf9de9d8bba9fe599eb63
Jared Crean <jcrean01@gmail.com>
2015-12-30 22:41:14 -0500

    finshed cleaning up ic.jl

--- src/solver/euler/ic.jl
+++ src/solver/euler/ic.jl
@@ -58,7 +58,7 @@ end  # end function
 
 
 @doc """
-### EulerEquationMod.ICZero
+### EulerEquationMod.ICOnes
 
   Sets all components of the solution to 1.0
 
@@ -109,7 +109,29 @@ function ICOnes{Tmsh, Tsbp, Tsol}(mesh::AbstractMesh{Tmsh},
   return nothing
 end # end function ICOnes
 
-function ICRho1E2{Tmsh, Tsbp, Tsol}(mesh::AbstractMesh{Tmsh}, operator::SBPOperator{Tsbp}, eqn::EulerData{Tsol}, opts, u0::AbstractVector{Tsol})
+
+@doc """
+### EulerEquationMod.ICRho1E2
+
+  Sets all density values to 1.0 and energy values to 2.0
+
+  Inputs:
+    mesh
+    sbp
+    eqn
+    opts
+
+  Inputs/Outputs: 
+    u0: vector to populate with the solution
+
+  Aliasing restrictions: none.
+
+"""->
+
+
+function ICRho1E2{Tmsh, Tsbp, Tsol}(mesh::AbstractMesh{Tmsh}, 
+                  operator::SBPOperator{Tsbp}, eqn::EulerData{Tsol}, opts, 
+                  u0::AbstractVector{Tsol})
 # populate u0 with initial values
 # this is a template for all other initial conditions
 
@@ -142,8 +164,29 @@ return nothing
 
 end  # end function
 
+@doc """
+### EulerEquationMod.ICRho1E2U3
 
-function ICRho1E2U3{Tmsh, Tsbp, Tsol}(mesh::AbstractMesh{Tmsh}, operator::SBPOperator{Tsbp}, eqn::EulerData{Tsol}, opts, u0::AbstractVector{Tsol})
+  Sets all components density values to 1.0, x and y momenti to 0.35355, and
+  energy to 2.0
+
+  Inputs:
+    mesh
+    sbp
+    eqn
+    opts
+
+  Inputs/Outputs: 
+    u0: vector to populate with the solution
+
+  Aliasing restrictions: none.
+
+"""->
+
+
+function ICRho1E2U3{Tmsh, Tsbp, Tsol}(mesh::AbstractMesh{Tmsh}, 
+                    operator::SBPOperator{Tsbp}, eqn::EulerData{Tsol}, 
+                    opts, u0::AbstractVector{Tsol})
 # populate u0 with initial values
 # this is a template for all other initial conditions
 
@@ -179,8 +222,29 @@ return nothing
 
 end  # end function
 
+@doc """
+### EulerEquationMod.ICFreeStream
 
-function ICFreeStream{Tmsh, Tsbp, Tsol}(mesh::AbstractMesh{Tmsh}, operator::SBPOperator{Tsbp}, eqn::EulerData{Tsol}, opts, u0::AbstractVector{Tsol})
+  Sets all components of the solution to the free stream condition according
+  to the angle of attack and and Mach number.
+
+  Inputs:
+    mesh
+    sbp
+    eqn
+    opts
+
+  Inputs/Outputs: 
+    u0: vector to populate with the solution
+
+  Aliasing restrictions: none.
+
+"""->
+
+
+function ICFreeStream{Tmsh, Tsbp, Tsol}(mesh::AbstractMesh{Tmsh}, 
+                      operator::SBPOperator{Tsbp}, eqn::EulerData{Tsol}, opts, 
+                      u0::AbstractVector{Tsol})
 # populate u0 with initial values
 # this is a template for all other initial conditions
 
@@ -219,7 +283,9 @@ end  # end function
 
 
 # what is this? how is it different than ICIsentropic Vortex?
-function ICVortex{Tmsh, Tsbp, Tsol}(mesh::AbstractMesh{Tmsh}, operator::SBPOperator{Tsbp}, eqn::EulerData{Tsol}, opts, u0::AbstractVector{Tsol})
+function ICVortex{Tmsh, Tsbp, Tsol}(mesh::AbstractMesh{Tmsh}, 
+                  operator::SBPOperator{Tsbp}, eqn::EulerData{Tsol}, opts, 
+                  u0::AbstractVector{Tsol})
 # populate u0 with initial values
 # this is a template for all other initial conditions
 
@@ -255,44 +321,27 @@ return nothing
 
 end  # end function
 
+@doc """
+### EulerEquationMod.ICsmoothHeavisideder
 
+  Sets the density to the derivative of the smooth Heaviside function, all 
+  other components to zero.
 
-#=
-function ICLinear{Tmsh, Tsbp, Tsol}(mesh::AbstractMesh{Tmsh}, operator::SBPOperator{Tsbp}, eqn::EulerData{Tsol}, opts, u0::AbstractArray{Tsol,1})
-# populate u0 with initial values
-# this is a template for all other initial conditions
+  Inputs:
+    mesh
+    sbp
+    eqn
+    opts
 
-nnodes = operator.numnodes
-dofpernode = getNumDofPerNode(mesh)
-dofnums_i = zeros(Int, dofpernode)
+  Inputs/Outputs: 
+    u0: vector to populate with the solution
 
-cntr = 1
-for i=1:mesh.numVert
-  for j=1:dofpernode
-    dofnums_i[j] = getNumberJ(mesh.dofnums_Nptr, mesh.verts[i], 0, j-1)
-  end
+  Aliasing restrictions: none.
 
-      dofnum_rho = dofnums_i[1]
-      dofnum_rhou = dofnums_i[2]
-      dofnum_rhov = dofnums_i[3]
-      dofnum_e = dofnums_i[4]
-
-
-      # apply initial conditions here
-      u0[dofnum_rho] = cntr
-      u0[dofnum_rhou] = cntr+1
-      u0[dofnum_rhov] = cntr+2
-      u0[dofnum_e] = cntr+3
-
-      cntr += 4
-end
-
-return nothing
-
-end  # end function
-=#
-
-function ICsmoothHeavisideder{Tmsh, Tsbp, Tsol}(mesh::AbstractMesh{Tmsh}, operator::SBPOperator{Tsbp}, eqn::EulerData{Tsol}, opts, u0::AbstractVector{Tsol})
+"""->
+function ICsmoothHeavisideder{Tmsh, Tsbp, Tsol}(mesh::AbstractMesh{Tmsh}, 
+                              operator::SBPOperator{Tsbp}, eqn::EulerData{Tsol},
+                              opts, u0::AbstractVector{Tsol})
 # calculate the value of the smooth heaviside function derivative at a location x
 # x0 is specified within this function
 
@@ -337,7 +386,28 @@ return nothing
 
 end
 
-function ICsmoothHeaviside{Tmsh, Tsbp, Tsol}(mesh::AbstractMesh{Tmsh}, operator::SBPOperator{Tsbp}, eqn::EulerData{Tsol}, opts, u0::AbstractArray{Tsol, 1})
+
+@doc """
+### EulerEquationMod.ICZero
+
+  Sets the density to the smooth Heaviside function, all other components to
+  zero.
+
+  Inputs:
+    mesh
+    sbp
+    eqn
+    opts
+
+  Inputs/Outputs: 
+    u0: vector to populate with the solution
+
+  Aliasing restrictions: none.
+
+"""->
+function ICsmoothHeaviside{Tmsh, Tsbp, Tsol}(mesh::AbstractMesh{Tmsh}, 
+                           operator::SBPOperator{Tsbp}, eqn::EulerData{Tsol}, 
+                           opts, u0::AbstractArray{Tsol, 1})
 # calculate the value of the smooth heaviside function at a location x
 # x0 is specified within this function
 
@@ -379,7 +449,26 @@ return nothing
 
 end
 
-function ICIsentropicVortex{Tmsh, Tsbp, Tsol}(mesh::AbstractMesh{Tmsh}, operator::SBPOperator{Tsbp}, eqn::EulerData{Tsol}, opts, u0::AbstractArray{Tsol})
+@doc """
+### EulerEquationMod.ICZero
+
+  Sets the solution to the isentropic vortex solution.
+
+  Inputs:
+    mesh
+    sbp
+    eqn
+    opts
+
+  Inputs/Outputs: 
+    u0: vector to populate with the solution
+
+  Aliasing restrictions: none.
+
+"""->
+function ICIsentropicVortex{Tmsh, Tsbp, Tsol}(mesh::AbstractMesh{Tmsh}, 
+                            operator::SBPOperator{Tsbp}, eqn::EulerData{Tsol}, 
+                            opts, u0::AbstractArray{Tsol})
 # populate u0 with initial values
 # this is a template for all other initial conditions
 
@@ -410,7 +499,29 @@ return nothing
 
 end  # end function
 
-function ICIsentropicVortexWithNoise{Tmsh, Tsbp, Tsol}(mesh::AbstractMesh{Tmsh}, operator::SBPOperator{Tsbp}, eqn::EulerData{Tsol}, opts, u0::AbstractVector{Tsol})
+
+@doc """
+### EulerEquationMod.ICZero
+
+  Sets the solutoin to the isentropic vortex solution plus a small random noise
+  component.
+
+  Inputs:
+    mesh
+    sbp
+    eqn
+    opts
+
+  Inputs/Outputs: 
+    u0: vector to populate with the solution
+
+  Aliasing restrictions: none.
+
+"""->
+function ICIsentropicVortexWithNoise{Tmsh, Tsbp, Tsol}(mesh::AbstractMesh{Tmsh},
+                                     operator::SBPOperator{Tsbp}, 
+                                     eqn::EulerData{Tsol}, 
+                                     opts, u0::AbstractVector{Tsol})
 # populate u0 with initial values
 # this is a template for all other initial conditions
 
@@ -436,8 +547,27 @@ return nothing
 
 end  # end function
 
+@doc """
+### EulerEquationMod.ICZero
 
-function ICUnsteadyVortex{Tmsh, Tsbp, Tsol}(mesh::AbstractMesh{Tmsh}, operator::SBPOperator{Tsbp}, eqn::EulerData{Tsol}, opts, u0::AbstractArray{Tsol})
+  Sets the solution to the unsteady vortex problem.  eqn.params.t is used to
+  determine what time to use for the solution.
+
+  Inputs:
+    mesh
+    sbp
+    eqn
+    opts
+
+  Inputs/Outputs: 
+    u0: vector to populate with the solution
+
+  Aliasing restrictions: none.
+
+"""->
+function ICUnsteadyVortex{Tmsh, Tsbp, Tsol}(mesh::AbstractMesh{Tmsh}, 
+                          operator::SBPOperator{Tsbp}, eqn::EulerData{Tsol}, 
+                          opts, u0::AbstractArray{Tsol})
 # populate u0 with initial values
 # this is a template for all other initial conditions
 
@@ -451,7 +581,7 @@ for i=1:numEl
   for j=1:nnodes
       dofnums_j = view(mesh.dofs, :, j, i)
  
-      coords_j = view(mesh.coords, :,j, i)
+      coords_j = view(mesh.coords, :, j, i)
       calcUnsteadyVortex(coords_j, eqn.params, sol)
 
       for k=1:dofpernode
@@ -466,9 +596,34 @@ return nothing
 end  # end function
 
 
+@doc """
+### EulerEquationMod.ICZero
+
+  This function reads a vector from a file on disk and set the solution to it.
+  The vector must contain the same number of entries as there are degrees of 
+  freedom in the mesh. 
+
+  This function is useful for things like restarting from a checkpoint.
+  In this case, the file should be the output of writedlm(eqn.q).  The degree 
+  of freedom number must be the same for both simulation for this to work (the 
+  file contains no degree of freedom number information).
 
 
-function ICFile{Tmsh, Tsbp, Tsol}(mesh::AbstractMesh{Tmsh}, operator::SBPOperator{Tsbp}, eqn::EulerData{Tsol}, opts, u0::AbstractVector{Tsol})
+  Inputs:
+    mesh
+    sbp
+    eqn
+    opts
+
+  Inputs/Outputs: 
+    u0: vector to populate with the solution
+
+  Aliasing restrictions: none.
+
+"""->
+function ICFile{Tmsh, Tsbp, Tsol}(mesh::AbstractMesh{Tmsh}, 
+                operator::SBPOperator{Tsbp}, eqn::EulerData{Tsol}, opts, 
+                u0::AbstractVector{Tsol})
 # populate u0 with initial values from a disk file
 # the file name comes from opts["ICfname"]
 
