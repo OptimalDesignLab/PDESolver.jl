c76d85dd01ee1105c925fd3d53b6da85d393b567
Jared Crean <jcrean01@gmail.com>
2016-01-12 21:07:39 -0500

    expanded build system

--- deps/build.jl
+++ deps/build.jl
@@ -1,41 +1,87 @@
 # download and build all non metadata dependences
+include("build_funcs")
 
-sbp_git = "https://github.com/OptimalDesignLab/SummationByParts.jl.git"
-pdesolvercommon_git = "https://github.com/OptimalDesignLab/ODLCommonTools.jl.git"
-pumiinterface_git = "https://github.com/OptimalDesignLab/PumiInterface.jl.git"
-petsc_git = "https://github.com/JaredCrean2/PETSc.jl.git"
-pkg_dict = Pkg.installed()  # get dictionary of installed package names to version numbers
+function installPDESolver()
 
-# generate the known_keys dictonary
-start_dir = pwd()
-input_path = joinpath(Pkg.dir("PDESolver"), "src/input")
-cd(input_path)
-include("extract_keys.jl")
-cd(start_dir)
+  f = open("install.log", "a+")
+  #------------------------------------------------------------------------------
+  # these packages are always installed manually
+  #-----------------------------------------------------------------------------
+  # [pkg_name, git url, commit identified]
+  std_pkgs = ["SummationByParts" "https://github.com/OptimalDesignLab/SummationByParts.jl.git" "b6cd5269738d43ddca7f2ae8586a67b8343af07c";
+              "ODLCommonTools" "https://github.com/OptimalDesignLab/ODLCommonTools.jl.git" "HEAD";
+              "PumiInterface" "https://github.com/OptimalDesignLab/PumiInterface.jl.git" "HEAD"
+              "MPI", "git@github.com:JuliaParallel/MPI.jl.git", "909b61426dc802a332109d729b8540f730377499"
+              ]
 
 
-if !haskey(pkg_dict, "ODLCommonTools")
-  Pkg.clone(pdesolvercommon_git)
-  Pkg.build("ODLCommonTools")
-end
+  # manually install MPI until the package matures
+  # handle Petsc specially until we are using the new version
+  petsc_git = "git@github.com:JaredCrean2/Petsc.git"
 
-if !haskey(pkg_dict, "SummationByParts")
-  Pkg.clone(sbp_git)
-  Pkg.build("SummationByParts")
-end
+  pkg_dict = Pkg.installed()  # get dictionary of installed package names to version numbers
 
-if !haskey(pkg_dict, "PumiInterface")
-  Pkg.clone(pumiinterface_git)
-  Pkg.build("PumiInterface")
-end
-
-if !haskey(pkg_dict, "PETSc")
-  Pkg.clone(petsc_git)
-  petsc_dir = Pkg.dir("PETSc")
+  # generate the known_keys dictonary
   start_dir = pwd()
-  cd(petsc_dir)
-  run(`git checkout 58999c0`)
+  input_path = joinpath(Pkg.dir("PDESolver"), "src/input")
+  cd(input_path)
+  include("extract_keys.jl")
   cd(start_dir)
-end
+
+  # force installation to a specific commit hash even if package is already present
+  # or would normally be install via the REQUIRE file
+  global const FORCE_INSTALL_ALL = haskey(ENV, "PDESOLVER_FORCE_DEP_INSTALL_ALL")
+
+  for i=1:size(std_pkgs, 1)
+    pkg_name = std_pkgs[i, 1]
+    git_url = std_pkgs[i, 2]
+    git_commit = std_pkgs[i, 3]
+
+    install_pkg(pkg_name, git_url, git_commit, pkg_dict, f)
+  end
 
 
+  # now install PETSc
+  pkg_name = "PETSc"
+  git_url = petsc_git
+  if !haskey(pkg_dict, pkg_name)  || haskey(ENV, "PDESOLVER_FORCE_DEP_INSTALL_$pkgname")  || FORCE_INSTALL_ALL
+    start_dir = pwd()
+    cd(Pkg.dir() )
+    run(`git clone $petsc_git`)
+    mv("./Petsc ./PETSc")
+    Pkg.build("PETSc")
+    cd(start_dir)
+  end
+
+  #------------------------------------------------------------------------------
+  # these packages are not always installed manually
+  # this section provides a backup mechanism if the regular version resolution
+  # mechanism fails
+  #------------------------------------------------------------------------------
+
+  # array of [pkg_name, commit_hash]
+  pkg_list = [ "Compat" "3bbd9536360ac0925b4535036b79d8eb547db977";
+               "URIParser" "3c00f48119909eb105aff6f126611b39a7b16919";
+               "FactCheck" "e3caf3143b13d4ce9bc576579d5ed9cd4aaefb11";
+               "ArrayViews" "40439daf99155b751d53c6403a5bd35fa0340f7a";
+               "SHA" "0e31e9d265eede8cafc9f0c04e928acf5b0fa4cf";
+               "BinDeps" "d57430a7e55546b9545054cfb5c34724dbed1795";
+               "NanMath" "b2148ab8461d73563dd4b7033e9a6546ec355328";
+               "Calculus" "4548c2204988c0d6551b0a7605ad3f4dee17b702";
+               "DualNumbers" "a9a47656fb6b70e5788551696f133cbe7886ea7b";
+               "ForwardDiff" "6da5f5204fd717a1417a8a4a6b9e2253799879c9";
+               "Debug" " 8c4801a6ca6368c6b5175c18a9d666a5694c1c3b"
+               ]
+
+  if haskey(ENV, "PDESOLVE_INSTALL_DEPS_MANUAL")
+    for i=1:size(pkg_list, 1)
+      pkg_name = std_pkgs[i, 1]
+      git_url = std_pkgs[i, 2]
+      git_commit = std_pkgs[i, 3]
+
+      install_pkg(pkg_name, git_url, git_commit, pkg_dict, f, force=true)
+    end
+  end
+
+  close(f)
+end  # end function
---
+++ deps/build_funcs.jl
@@ -0,0 +1,36 @@
+
+function set_hash(pkg_name::AbstractString, hash::AbstractString)
+# this function checks out a particular git hash of a particular package
+
+  start_dir = pwd()
+  cd(Pkg.dir(pkg_name))
+  run(`git checkout -b pdesolver_version $hash`)
+  cd(start_dir)
+
+end
+
+function install_pkg(pkg_name::AbstractString, git_url::AbstractString, git_commit::AbstractString, pkg_dict, f; force=false)
+# pkg_name = name of package
+# git_url: value to be passed to Pkg.clone()
+# git_commit: commit has to check out
+# pkg_dict: output of Pkg.installed, passed as an argument because it takes so long to get
+# f: file handle to write progress to
+# force: install the package regardless of other flags, default false
+
+  if !haskey(pkg_dict, pkg_name)  || haskey(ENV, "PDESOLVER_FORCE_DEP_INSTALL_$pkgname")  || FORCE_INSTALL_ALL || force
+    println(f, "Installing package $pkg_name")
+    try 
+      Pkg.clone(git_url)
+      set_has(git_url)
+      Pkg.build(pkg_name)
+      println(f, "  Installation appears to have completed sucessfully")
+    catch
+      println(f, "Error installing package $pkg_name")
+    end
+
+  else
+    println(f, "Skipping installation of package $pkg_name")
+  end
+
+  flush(f)
+end
