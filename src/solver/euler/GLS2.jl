a049a80d1e3b1c9543f4c6b6e2cb3811f740b910
Jared Crean <jcrean01@gmail.com>
2016-01-15 15:12:53 -0500

    first draft of GLS finished

--- src/solver/euler/GLS2.jl
+++ src/solver/euler/GLS2.jl
@@ -10,7 +10,7 @@ function applyGLS2{Tmsh, Tsol, Tres, Tdim}(mesh::AbstractMesh{Tmsh},
   # extract some constants
   numDofPerNode = mesh.numDofPerNode
   numNodesPerElement = mesh.numNodesPerElement
-
+  w = sbp.w
   # reusable storage
 
   # flux jacobians: a n x n matrix for each coordinate direction for each node
@@ -21,38 +21,114 @@ function applyGLS2{Tmsh, Tsol, Tres, Tdim}(mesh::AbstractMesh{Tmsh},
   # temporarily hold the transposed q variables for the element
   qtranspose = zeros(Tsol, mesh.numNodesPerElement, mesh.numDofPerNode)
   # temporarily hold the result of D*qtranspose
-  qxtranspose = zeros(Tsol, mesh.numNodesPerElement, mesh.numDofPerNode, Tdim)
-  # hold un-transposed qx
-  qx = zeros(Tsol, mesh.numDofPerNode, mesh.numNodesPerElement, Tdim)
+  qxitranspose = zeros(Tsol, mesh.numNodesPerElement, mesh.numDofPerNode, Tdim)
+  # hold un-transposed qxi (temporarily)
+  qxi = zeros(Tsol, mesh.numDofPerNode, mesh.numNodesPerElement, Tdim)
   # hold tau, a n x n matrix for each node
   tau = zeros(Tres, mesh.numDofPerNode, mesh.numDofPerNode, 
                     mesh.numNodesPerElement)
-
   # hold differentiation matrices D
   D = zeros(Float64, numNodesPerElement, numNodesPerElement, Tdim)
+  Dtranspose = zeros(D)  # transpose D because we access it in that order
   # temporary vectors 1 through 4, stored in a matrix so they can be
   # access programaticaly
   tmps = zeros(Tres, mesh.numDofPerNode, 2*Tdim)
+  qx = zeros(Tres, numDofPerNode, Tdim)  # accumulation vectors
 
   # calculate D
   for d=1:Tdim
     smallmatmat!(diagm(sbp.w), view(sbp.Q, :, :, d), view(D, :, :, d))
+    Dtranspose[:, :, d] = D[:, :, d].'
   end
+
+  
 
   for el = 1:mesh.numEl
     # get all the quantities for this element
     dxidx_hat = view(mesh.dxidx, :, :, :, el)
     getGLSVars(eqn.params, view(q, :, :, el), dxidx_hat, view(mesh.jac, :, el),
-               D, A_mats, qtranspose, qxtranspose, qx, dxidx, tau)
+               D, A_mats, qtranspose, qxitranspose, qxi, dxidx, tau)
+
+    for i=1:numNodesPerElement
+      res_i = view(eqn.res, :, i, el)
+      for j=1:numNodesPerElement
+
+        # rotate qxi, qeta to qx, qy
+        for d1=1:Tdim  # the x-y coordinate direction
+          for d2=1:Tdim  # the xi-eta coordinate direction
+            for n=1:numDofPerNode
+              qx[n, d1] += dxidx_hat[d2, d1, j]*qxi[n, d2]
+            end
+          end
+        end
+
+        # multiply qx, qy  flux jacobians Ax, Ay
+        for d1=1:Tdim
+          A_d1 = view(A_mats, :, :, d1, j)
+          q_d1 = view(qx, :, d1)
+          tmp_d1= view(tmps, d1)
+          smallmatvec!(Ad1, q_d1, tmp_d1)
+        end
+
+        # now add them together
+        tmp1 = view(tmps, :, 1)
+        for d1=2:Tdim
+          tmp_other = view(tmps, :, d1)
+          for n=1:numDofPerNode
+            tmp1[n] += tmp_other[n]
+          end
+        end
+
+        # now multiply by tau
+        tau_j = view(taus, :, :, j)
+        tmp2 = view(tmps, :, 2)  # store result of multiplication here
+        smallmatvec!(tau_j, tmp1, tmp2)  # this overwrites tmp2
+
+        # copy tmp2 into tmp1 so the next phase of reductions can proceed in
+        # parallel
+        # multiply by integration weight (from sbp.w) at the same time
+        for n=1:numDofPerNode
+          tmp2[n] *= w[n]
+          tmp1[n] = tmp2[n]
+        end
+
+        # now do weighting space 
+         # rotate the differentiation matrices D into x-y
+        for d1=1:Tdim  # the x-y coordinate direction
+          fac = zero(Tmsh)  # variable to accumulate the factor in
+          for d2=1:Tdim  # the xi-eta coordinate direction
+              fac += dxidx_hat[d2, d1, j]*Dtranspose[i, j, d2]
+          end
+
+          # now update tmp1 through tmp 2/3 with the factor
+          for n=1:numDofPerNode
+            tmp[n, d1] *= fac
+          end
+        end
+
+       # now multiply by flux jacobians transposed
+       for d1=1:Tdim
+         A_d1 = view(A_mats, :, :, d1)
+         x_d1 = view(tmp, :, d1)
+         b_d1 = view(tmp, :, d1+Tdim)  # now we use the second half of tmps
+         smallmatTvec!(A_d1, x_d1, b_d1)
+       end
+
+       # now update res
+       for d1=(Tdim+1):(2*Tdim)  # loop over the second half of tmps
+         @simd for n=1:numDofPerNode
+           res[n] += tmps[n, d1]
+         end
+       end
 
 
 
+      end  # end loop over i
+    end  # end loop over j
 
+  end  # end loop over elements
 
-
-
-  end
-
+  return nothing
 
 end  # end function
 
@@ -81,9 +157,9 @@ end  # end function
     A_mats: array the be populated with flux jacobian for the entire element,
             (numDofPerNode x NumDofPerNode) x Tdim x numNodesPerElement
     qtranspose: array to hold transpose(q) (temporarily)
-    qxtranspose: array to hold [Dxi*transpose(q), Deta*transpose(q) ...]
+    qxitranspose: array to hold [Dxi*transpose(q), Deta*transpose(q) ...]
                  numNodesPerElement x numDofPerNode x Tdim  (temporarily)
-    qx: transpose of qxtranspose (reversing the first two dimensions)
+    qxi: transpose of qxtranspose (reversing the first two dimensions)
     dxidx: dxidx_hat * |J| (so the scaling by 1/|J| is undone)
     tau:  array of tau matrices for each node, 
           numDofPerNode x numDofPerNode x numNodesPerElement
@@ -97,8 +173,8 @@ function getGLSVars{Tmsh, Tsol, Tres, Tdim}(params::ParamType{Tdim},
                     D::AbstractArray{Float64, 3}  
                     A_mats::AbstractArray{Tsol, 4}, # begin outputs
                     qtranspose::AbstractArray{Tsol, 2}, 
-                    qxtranspose::AbstractArray{Tsol, 3}, 
-                    qx::AbstractArray{Tsol, 3},
+                    qxitranspose::AbstractArray{Tsol, 3}, 
+                    qxi::AbstractArray{Tsol, 3},
                     dxidx::AbstractArray{Tmsh, 3}, 
                     tau::AbstractArray{Tres, 3})
 
@@ -121,7 +197,7 @@ function getGLSVars{Tmsh, Tsol, Tres, Tdim}(params::ParamType{Tdim},
   for d=1:Tdim
     for j=1:numNodesPerElement
       for i=1:numDofPerNode
-        qx[i, j, d] = qxtranpose[j, i, d]
+        qxi[i, j, d] = qxitranpose[j, i, d]
       end
     end
   end
