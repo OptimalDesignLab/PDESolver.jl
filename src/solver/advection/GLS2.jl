c9b9c43d74ed140d0f9b5ff5c1030ef1c941e9c2
Jared Crean <jcrean01@gmail.com>
2016-02-16 23:12:21 -0500

    cleanup advection GLS, add docs

--- src/solver/advection/GLS2.jl
+++ src/solver/advection/GLS2.jl
@@ -1,6 +1,19 @@
+@doc """
+### AdvectionEquationMod.applyGLS2
 
+  This function updates the residual with the GLS term, using the definition
+  of the parameter tau from Hughes Part III.
 
-function applyGLS2{Tmsh, Tsol, Tres}(mesh::AbstractMesh{Tmsh}, sbp::SBPOperator, eqn::AdvectionData{Tsol, Tres, 2}, opts, src_func)
+  Inputs:
+    mesh
+    sbp
+    eqn
+    opts
+    src_func:  The functor that calculates the source term at a node
+
+  Aliasing restrictions: none
+"""->
+function applyGLS2{Tmsh, Tsol, Tres}(mesh::AbstractMesh{Tmsh}, sbp::SBPOperator, eqn::AdvectionData{Tsol, Tres, 2}, opts, src_func::SRCType)
 
 #  println("----- entered applyGLS2 -----")
   Dx = zeros(Float64, mesh.numNodesPerElement, mesh.numNodesPerElement)
@@ -17,17 +30,6 @@ function applyGLS2{Tmsh, Tsol, Tres}(mesh::AbstractMesh{Tmsh}, sbp::SBPOperator,
   # calculate Dxi and Deta
   Dxi = diagm(1./sbp.w)*sbp.Q[:, :, 1]
   Deta = diagm(1./sbp.w)*sbp.Q[:, :, 2]
-
-  #DEBUGGING
-  gls_res = zeros(Tres, mesh.numDofPerNode, mesh.numNodesPerElement, mesh.numEl)
-  gls_full = zeros(Tres, mesh.numDofPerNode, mesh.numNodesPerElement, mesh.numEl)
-  middle_term = zeros(Tres, mesh.numDofPerNode, mesh.numNodesPerElement, mesh.numEl)
-  res_before = copy(eqn.res)
-  weighting_res = zeros(middle_term)
-  weighting_vec = ones(mesh.numNodesPerElement)
-  tau_sum = zero(Tres)
-  tau_cnt = 0
-
 
   for i=1:mesh.numEl
     # calculate the quantities needed for this element
@@ -62,9 +64,6 @@ function applyGLS2{Tmsh, Tsol, Tres}(mesh::AbstractMesh{Tmsh}, sbp::SBPOperator,
 
      tau_vec[j] = getTau(alpha_x, alpha_y, jac[j], mesh.min_node_dist)
 #     println("tau_vec[$j] = ", tau_vec[j])
-     #DEBUGGING
-     tau_sum += tau_vec[j]
-     tau_cnt += 1
    end
 
    for j=1:mesh.numNodesPerElement
@@ -88,23 +87,12 @@ function applyGLS2{Tmsh, Tsol, Tres}(mesh::AbstractMesh{Tmsh}, sbp::SBPOperator,
      red_vec1[j] -= src_func(coords_j, alpha_x, alpha_y, eqn.t)
    end
 
-   #DEBUGGING
-   for j=1:mesh.numNodesPerElement
-     gls_res[1, j, i] = red_vec1[j]
-   end
-
-   
-#   println("weighting space term = ", red_vec1)
    # middle terms
    # also copy pinto red_vec2 at same time
    for j=1:mesh.numNodesPerElement
      red_vec1[j] *= tau_vec[j]*sbp.w[j]/jac[j]
      red_vec2[j] = red_vec1[j]
-     #DEBUGGING
-     middle_term[1, j, i] = red_vec1[j]
    end
-
-#   println("after middle term = ", red_vec1)
 
    smallmatTvec!(Dx, red_vec1, red_vec3)
    smallmatTvec!(Dy, red_vec2, red_vec4)
@@ -114,69 +102,38 @@ function applyGLS2{Tmsh, Tsol, Tres}(mesh::AbstractMesh{Tmsh}, sbp::SBPOperator,
    end
 
 
-   weight_vals = alpha_x*smallmatTvec(Dx, weighting_vec)
-   weight_vals2 =alpha_y*smallmatTvec(Dy, weighting_vec)
-
-
-   # add source term to weighting term
-   for j=1:mesh.numNodesPerElement
-#     coords_j = view(mesh.coords, :, j, i)
-#     q_j = u[j]
-#     red_vec3[j] -= (src_func(coords_j, alpha_x, alpha_y, eqn.t)/q_j)*red_vec1[j]
-#      red_vec3[j] = red_vec1[j]
-      #DEBUGGING
-      weighting_res[1, j, i] = weight_vals[j] + weight_vals2[j]
-   end
-
-
    # update res
    for j=1:mesh.numNodesPerElement
      res[j] -= red_vec3[j]
-     gls_full[1, j, i] -= red_vec3[j]
    end
 
 
   end  # end loop over elements
 
-  gls_resvec = zeros(Tsol, mesh.numDof)
-  full_resvec = zeros(Tsol, mesh.numDof)
-  middle_vec = zeros(Tsol, mesh.numDof)
-  old_resvec = zeros(Tsol, mesh.numDof)
-  weight_resvec = zeros(Tsol, mesh.numDof)
-  assembleSolution(mesh, sbp, eqn, opts, gls_res, gls_resvec)
-  assembleSolution(mesh, sbp, eqn, opts, gls_full, full_resvec)
-  assembleSolution(mesh, sbp, eqn, opts, middle_term, middle_vec)
-  assembleSolution(mesh, sbp, eqn, opts, weighting_res, weight_resvec)
-#  writedlm("gls_full.dat", real(gls_full))
-#  writedlm("gls_fullvec.dat", full_resvec)
-  assembleSolution(mesh, sbp, eqn, opts, res_before,  old_resvec)
-  gls_norm = norm(gls_resvec, Inf)
-  full_norm = norm(full_resvec, Inf)
-  middle_norm = norm(middle_vec, Inf)
-  old_norm = norm(old_resvec, Inf)
-  weight_norm = norm(weight_resvec, Inf)
-#  gls_norm = calcNorm(eqn, gls_resvec)
-#  full_norm = calcNorm(eqn, full_resvec, strongres=true)
-#  middle_norm = calcNorm(eqn, middle_vec, strongres=false)
-#  old_norm = calcNorm(eqn, old_resvec, strongres=true)
-#  weight_norm = calcNorm(eqn, weight_resvec, strongres=false)
-  tau_avg = tau_sum/tau_cnt
-  rmfile("gls_norm.dat")
-  f = open("gls_norm.dat", "w")
-  println(f, gls_norm, " ", old_norm, " ", full_norm, " ", real(tau_avg), " ", middle_norm, " ", weight_norm)
-  close(f)
 
-
-
-
-#  println("----- Finished applyGLS2 -----")
+  #  println("----- Finished applyGLS2 -----")
   return nothing
 
 end  # end function
 
 
 
+@doc """
+### AdvectionEquationMod.getTau
 
+  This function calculates the stabilization parameter tau for GLS at a node, 
+  using the definition given in Hughes Part III
+
+  Inputs:
+    alpha_x: advection coefficient in the x direction at the node
+    alpha_y: advection coefficient in the y direction at the node
+    dxidx: a 2x2 matrix containing the mapping jacobian at the node
+    p : which p norm to use (typically either 1 or 2)
+
+  Outputs:
+    tau: the value of tau at the node
+
+"""->
 function getTau(alpha_x, alpha_y, dxidx::AbstractMatrix, p)
   fac = 1.0
   b1 = dxidx[1,1]*alpha_x + dxidx[1,2]*alpha_y
@@ -185,6 +142,20 @@ function getTau(alpha_x, alpha_y, dxidx::AbstractMatrix, p)
   return fac*(bp^(-1/p))
 end
 
+@doc """
+### AdvectionEquationMod.getTau
+
+  This function calculate the stabilization parameter tau at a node using 
+  the (heuristic) definition 0.5*h/sqrt(alpha_x^2 + alpha_y^2)
+
+  Inputs:
+    alpha_x: advection coefficient in the x direction at the node
+    alpha_y: advection coefficient in the y direction at the node
+    jac: determinant of the mapping jacobian at the node
+
+  Outputs:
+    tau: the value of tau at the node
+"""->
 function getTau{Tmsh}(alpha_x, alpha_y, jac::Tmsh, min_node_dist)
 
   fac = 1
