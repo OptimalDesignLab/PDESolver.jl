0e89eec4aae54f4db7f6e09f5c3198884f3c45f2
Kinshuk Panda <pandak@clue.scorec.rpi.edu>
2016-04-13 17:54:00 -0400

    added a method to compute the derivative of functional wrt q_bndry ina vector form - untested

--- src/solver/advection/adjoint.jl
+++ src/solver/advection/adjoint.jl
@@ -40,7 +40,6 @@ function calcAdjoint{Tmsh, Tsol}(mesh::AbstractDGMesh{Tmsh},sbp::AbstractSBP,
   # the functional edges
   n_functional_faces = 0  # Total length of the interpolated q values across all geometric functional edges
   for i = 1:length(functional_edges)
-    nfaces = 0
     g_edge_number = functional_edges[i]
     start_index = mesh.bndry_offsets[g_edge_number]
     end_index = mesh.bndry_offsets[g_edge_number+1]
@@ -49,56 +48,58 @@ function calcAdjoint{Tmsh, Tsol}(mesh::AbstractDGMesh{Tmsh},sbp::AbstractSBP,
     n_functional_faces += nfaces
   end  # End for i = 1:length(functional_edges)
   
-  # create an array to store all q_bndry values for that functional 
-  fq_bndry = zeros(Tsol,n_functional_faces)
+  # Create a book-keeping array of tuples for storing information on 
+  # 1. sbpface.numnodes index
+  # 2. global facenum
+  # 3. functional edge number
 
-  # Populate fq_bndry
-  for i = 1:length(functional_edges)
-    g_edge_number = functional_edges[itr] # Extract geometric edge number
+  fq_bndry_info = Array{Tuple{Int, Int, Int}}(n_functional_faces) # Creat
+  starting_index = 0
+  for itr = 1:length(functional_edges)
+    g_edge_number = functional_edges[itr]
     start_index = mesh.bndry_offsets[g_edge_number]
     end_index = mesh.bndry_offsets[g_edge_number+1]
     idx_range = start_index:(end_index-1)
     bndry_facenums = view(mesh.bndryfaces, idx_range) # faces on geometric edge i
     nfaces = length(bndry_facenums)
 
-    for j = 1:nfaces
-      bndry_j = bndry_facenums[j]
-      global_facenum = idx_range[j]
-      for k  = 1:mesh.sbpface.numnodes
-        # indexing to jump nfaces per geometric edge and populate q_values
-        fq_bndry[(i-1)*nfaces + k] = eqn.q_bndry[1, k, global_facenum]
-      end  # End for j = 1:nfaces
-    end    # End for j = 1:nfaces
-  end      # End for i = 1:length(functional_edges)
-
-  return nothing
+    for i = 1:nfaces
+      bndry_i = bndry_facenums[i]
+      global_facenum = idx_range[i]
+      for j = 1:mesh.sbpface.numnodes
+        fq_bndry_info[starting_index + (i-1)*mesh.sbpface.numnodes + j] = 
+                                               (j,global_facenum,g_edge_number)
+      end
+    endc
+    nfaces_prev = nfaces
+    starting_index += nfaces_prev*mesh.sbpface.numnodes
+  end
 end  # End function calcAdjoint
 
-function calcFunctionalDeriv(mesh, sbp, eqn, opts, fq_bndry)
+function calcFunctionalDeriv(mesh, sbp, eqn, opts, fq_bndry, functional_edges)
+  
+  # create a 1D array of length n_functional_faces to store df/dq_bndry
+  df_dqbndry = zeros(n_functional_faces)
 
-  entry_orig = zero(eltype(fq_bndry))
+  pert = complex(0, opts["epsilon"])  # complex perturbation
 
-  for i = 1:length(fq_bndry)
-    if i == 1
-      entry_orig = fq_bndry[i]
-      fq_bndry[i] += pert
-    else
-      fq_bndry[i-1] = entry_orig # Undo previous iteration perturbation
-      entry_orig = fq_bndry[i]
-      fq_bndry[i] += pert
-    end
+  # populate df_dqbndry
+  for i = 1:n_functional_faces
+     
+    # get q_bndry
+    sbpface_index, global_facenum, g_edge_number = fq_bndry_info[i]
 
-    # Compute the functional with perturbed q_bndry
-    # - This would require disassembly of fq_bndry to q_bndry
-    # - supply q_bndry to a function that computes the functional value
-    # 
-    # use that to compute the scomplex step derivative using calcJacCol
+    # perturb the q_bndry corresponding to the index
+    eqn.q_bndry[1,sbpface_index,global_facenum] += pert
 
+    # Use the above perturbed value to get the functional value
+    functional_val = calcBndryfunctional(mesh, ebp, eqn, opts, functional_edges)
+    df_dqbndry[i] = imag(functional_val)/norm(pert)
+    
+  end  # end for i = 1:n_functional_faces
 
-  end  # end for i = 1:length(fq_bndry)
-
-  # Undo final perturbation
-  fq_bndry[i] = entry_orig
+  # unperturb this eqn.q_bndry
+  eqn.q_bndry[1,sbpface_index,global_facenum] -= pert
 
   return nothing
 end
