053b0b45e21ef1d54b85eb8bd275f0cd5f5e7baa
Kinshuk Panda <pandak@monopoly.scorec.rpi.edu>
2016-04-27 13:36:25 -0400

    added test for functional integration for DG euler

---
+++ test/test_adjoint.jl
@@ -0,0 +1,20 @@
+# Test functional Integrate and adjoint for euler equation.
+
+ARGS[1] = "input_vals_vortex_adjoint_DG.jl"
+include("../src/solver/euler/startup.jl")  # initialization and construction
+
+facts("--- Testing Functional Computation On a Boundary ----") do
+
+  @fact mesh.isDG --> true
+  @fact opts["functional_name1"] --> "drag"
+  @fact opts["analytical_functional_val"] --> roughly(-1/1.4, atol = 1e-13)
+  @fact opts["geom_edges_functional1"] --> [4]
+
+  fname = "./functional_error1.dat"
+  relative_error = readdlm(fname)
+
+  @fact relative_error[1] --> roughly(0.00018849571, atol = 1e-6)
+
+  rm("./functional_error1.dat") # Delete the file
+
+end  # End do
