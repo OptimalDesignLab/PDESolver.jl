880844374ecceb9c0bb415b364d89b6a91a86843
Jared Crean <jcrean01@gmail.com>
2016-05-28 21:53:46 -0400

    add perf history files

--- perf/newton/runtest.jl
+++ perf/newton/runtest.jl
@@ -1,13 +1,25 @@
-function runtest_rk(;fname="input_vals_vortex3.jl")
+using ODLCommonTools
+push!(LOAD_PATH, joinpath(Pkg.dir("PDESolver"), "src/Utils"))
+using Utils
+
+function runtest_rk(;fname="input_vals_vortex3.jl", outname="perf_hist.txt")
 
   startup_path = joinpath(Pkg.dir("PDESolver"), "src/solver/euler/startup.jl")
   resize!(ARGS, 1)
   ARGS[1] = fname
   println("----- warming up -----")
-  @time include(startup_path)
+  val, telapsed, tgc, gc_bytes = @time_all include(startup_path)
 
   println("----- Final run -----")
-  @time include(startup_path)
+  val, telapsed, tgc, gc_bytes = @time_all include(startup_path)
+
+  f = open(outname, "a+")
+  tstr = getTimeString()
+  branchname = getBranchName()
+  println(f, tstr, " ", branchname, " ", telapsed, " ", gc_bytes)
+  close(f)
+
+
 end
 
 runtest_rk()
