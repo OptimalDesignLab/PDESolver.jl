672e0f32244e7b619b1a4127d679b8c65d1a28ef
Jared Crean <jcrean01@gmail.com>
2016-07-21 23:25:57 -0400

    cleanup

--- src/solver/euler/common_funcs.jl
+++ src/solver/euler/common_funcs.jl
@@ -74,426 +74,36 @@ return nothing
 
 end
 
-#=
-function calcIsentropicVortex{Tmsh, Tsol}(coords::AbstractArray{Tmsh}, 
-                              params::ParamType{3}, sol::AbstractVector{Tsol})
-# calculates the solution at a point of the isentropic vortex
-
-
-# unpack arguments
-x = coords[1]; x_orig = x
-y = coords[2]; y_orig = y
-z = coords[3]; z_orig = z;
-#=
-println("x = ", x, ", z = ", z)
-
-phi_z = pi/4  # angle of axis relative to z axis
-
-# calculate new x coordinate in rotated coordinate system
-theta1 = atan2(z, x)
-#println("theta1 = ", theta1)
-phi2 = 0.5*pi - theta1
-#println("phi2 = ", phi2)
-r_xz = sqrt(x*x + z*z)  # distance from origin to point in xz plane
-#println("r_xz = ", r_xz)
-x = r_xz*sin(phi_z + phi2)  # x coordinate in rotated system
-println("x = ", x)
-### Debug ###
-lAB = r_xz*cos(phi_z + phi2)
-#println("lAB = ", lAB)
-theta2 = 0.5*pi - phi_z
-#println("theta2 = ", theta2)
-Bx = -lAB*cos(theta2)
-Bz = lAB*sin(theta2)
-println("Bx = ", Bx, ", Bz = ", Bz)
-theta3_debug = atan2(z_orig - Bz, x_orig - Bx)
-### end Debug ###
-
-theta3 = theta1 + phi_z + phi2 - 0.5*pi
-
-@assert abs(theta3_debug - theta3) < 1e-12
-=#
-#println("theta1 = ", theta1)
-#println("delta_theta = ", delta_theta)
-#println("x_orig = ", x, ", x_orig = ", x_orig)
-#println("z_orig = ", z_orig)
-#@assert abs(x - x_orig) < 1e-12
-# get some values out of eqn
-cv = params.cv
-R = params.R
-gamma = params.gamma
-
-# the (hard coded) parameters are
-r_in = 1  # inner radius of sector of circle
-rho_in = 2 # density at inner radius
-M_in = 0.95  # Mach number
-p_in =  1/gamma
-
-# calculate r, theta coordinates from x,y
-r = sqrt(x*x + y*y)
-theta = atan2(y,x)  # angle in radians
-# calculate values at r radius
-tmp1 = ((gamma-1)/2)*M_in*M_in
-rho_r = rho_in*(1 + tmp1*(1- (r_in*r_in)/(r*r)))^(1/(gamma-1))
-
-p_r = p_in*(rho_r/rho_in)^gamma
-
-a_r = sqrt( gamma*p_r/rho_r )
-
-M_r = sqrt( (2/(gamma-1))*((rho_in/rho_r)^(gamma-1))*(1 + tmp1) - 2/(gamma-1) )
-U_r = M_r*a_r  # velocity magnitude
-
-u_r = U_r*sin(theta)
-v_r = -U_r*cos(theta)
-w_r = 0.25
-e_r = cv*p_r/(rho_r*R)
-E_r = rho_r*e_r + 0.5*rho_r*U_r*U_r
-
-# convert velocities back to base frame of reference
-#u_r = u_r*cos(theta3)
-#w_r = u_r*sin(theta3)
-
-# save solution to sol
-sol[1] = rho_r
-sol[2] = rho_r*u_r
-sol[3] = rho_r*v_r
-sol[4] = rho_r*w_r
-sol[5] = E_r
-
-return nothing
-:
-end
-=#
-
-function calcIsentropicVortex1{Tmsh, Tsol}(coords::AbstractArray{Tmsh}, 
-                              params::ParamType{3}, sol::AbstractVector{Tsol})
-# calculates the solution at a point of the isentropic vortex
-
-println("--- entered calcIsentropicVortex1 ---")
-
-# unpack arguments
-x = coords[1]; x_orig = x
-y = coords[2]; y_orig = y
-z = coords[3]; z_orig = z;
-#x = z
-#=
-println("x = ", x, ", z = ", z)
-
-phi_z = pi/4  # angle of axis relative to z axis
-
-# calculate new x coordinate in rotated coordinate system
-theta1 = atan2(z, x)
-#println("theta1 = ", theta1)
-phi2 = 0.5*pi - theta1
-#println("phi2 = ", phi2)
-r_xz = sqrt(x*x + z*z)  # distance from origin to point in xz plane
-#println("r_xz = ", r_xz)
-x = r_xz*sin(phi_z + phi2)  # x coordinate in rotated system
-println("x = ", x)
-### Debug ###
-lAB = r_xz*cos(phi_z + phi2)
-#println("lAB = ", lAB)
-theta2 = 0.5*pi - phi_z
-#println("theta2 = ", theta2)
-Bx = -lAB*cos(theta2)
-Bz = lAB*sin(theta2)
-println("Bx = ", Bx, ", Bz = ", Bz)
-theta3_debug = atan2(z_orig - Bz, x_orig - Bx)
-### end Debug ###
-:q
-theta3 = theta1 + phi_z + phi2 - 0.5*pi
-
-@assert abs(theta3_debug - theta3) < 1e-12
-=#
-#println("theta1 = ", theta1)
-#println("delta_theta = ", delta_theta)
-#println("x_orig = ", x, ", x_orig = ", x_orig)
-#println("z_orig = ", z_orig)
-#@assert abs(x - x_orig) < 1e-12
-# get some values out of eqn
-cv = params.cv
-R = params.R
-gamma = params.gamma
-
-# the (hard coded) parameters are
-r_in = 1  # inner radius of sector of circle
-rho_in = 2 # density at inner radius
-M_in = 0.95  # Mach number
-p_in =  1/gamma
-
-# calculate r, theta coordinates from x,y
-r = sqrt(x*x + y*y)
-
-theta = atan2(y,x)  # angle in radians
-println("atan($y, $x) = ", theta)
-# calculate values at r radius
-tmp1 = ((gamma-1)/2)*M_in*M_in
-rho_r = rho_in*(1 + tmp1*(1- (r_in*r_in)/(r*r)))^(1/(gamma-1))
-
-p_r = p_in*(rho_r/rho_in)^gamma
-
-a_r = sqrt( gamma*p_r/rho_r )
-
-M_r = sqrt( (2/(gamma-1))*((rho_in/rho_r)^(gamma-1))*(1 + tmp1) - 2/(gamma-1) )
-U_r = M_r*a_r  # velocity magnitude
-
-u_r = U_r*sin(theta)
-v_r = -U_r*cos(theta)
-#v_r = U_r*sin(theta)
-#w_r = -U_r*cos(theta)
-println("u_r = ", u_r)
-println("v_r = ", v_r)
-w_r = 0.0
-e_r = cv*p_r/(rho_r*R)
-E_r = rho_r*e_r + 0.5*rho_r*U_r*U_r
-
-# convert velocities back to base frame of reference
-#u_r = u_r*cos(theta3)
-#w_r = u_r*sin(theta3)
-
-# save solution to sol
-sol[1] = rho_r
-sol[2] = rho_r*u_r
-sol[3] = rho_r*v_r
-sol[4] = rho_r*w_r
-sol[5] = E_r
-
-println("--- finished calcIsentropicVortex1 ---")
-return nothing
-
-end
-
-
-function calcIsentropicVortex2{Tmsh, Tsol}(coords::AbstractArray{Tmsh}, 
-                              params::ParamType{3}, sol::AbstractVector{Tsol})
-# calculates the solution at a point of the isentropic vortex
-
-println("--- entered calcIsentropicVortex2 ---")
-
-# unpack arguments
-x = coords[1]; x_orig = x
-y = coords[2]; y_orig = y
-z = coords[3]; z_orig = z;
-#x = z
-#=
-println("x = ", x, ", z = ", z)
-
-phi_z = pi/4  # angle of axis relative to z axis
-
-# calculate new x coordinate in rotated coordinate system
-theta1 = atan2(z, x)
-#println("theta1 = ", theta1)
-phi2 = 0.5*pi - theta1
-#println("phi2 = ", phi2)
-r_xz = sqrt(x*x + z*z)  # distance from origin to point in xz plane
-#println("r_xz = ", r_xz)
-x = r_xz*sin(phi_z + phi2)  # x coordinate in rotated system
-println("x = ", x)
-### Debug ###
-lAB = r_xz*cos(phi_z + phi2)
-#println("lAB = ", lAB)
-theta2 = 0.5*pi - phi_z
-#println("theta2 = ", theta2)
-Bx = -lAB*cos(theta2)
-Bz = lAB*sin(theta2)
-println("Bx = ", Bx, ", Bz = ", Bz)
-theta3_debug = atan2(z_orig - Bz, x_orig - Bx)
-### end Debug ###
-:q
-theta3 = theta1 + phi_z + phi2 - 0.5*pi
-
-@assert abs(theta3_debug - theta3) < 1e-12
-=#
-#println("theta1 = ", theta1)
-#println("delta_theta = ", delta_theta)
-#println("x_orig = ", x, ", x_orig = ", x_orig)
-#println("z_orig = ", z_orig)
-#@assert abs(x - x_orig) < 1e-12
-# get some values out of eqn
-cv = params.cv
-R = params.R
-gamma = params.gamma
-
-# the (hard coded) parameters are
-r_in = 1  # inner radius of sector of circle
-rho_in = 2 # density at inner radius
-M_in = 0.95  # Mach number
-p_in =  1/gamma
-
-# calculate r, theta coordinates from x,y
-r = sqrt(z*z + y*y)
-
-theta = atan2(z,y)  # angle in radians
-println("atan($z, $y) = ", theta)
-# calculate values at r radius
-tmp1 = ((gamma-1)/2)*M_in*M_in
-rho_r = rho_in*(1 + tmp1*(1- (r_in*r_in)/(r*r)))^(1/(gamma-1))
-
-p_r = p_in*(rho_r/rho_in)^gamma
-
-a_r = sqrt( gamma*p_r/rho_r )
-
-M_r = sqrt( (2/(gamma-1))*((rho_in/rho_r)^(gamma-1))*(1 + tmp1) - 2/(gamma-1) )
-U_r = M_r*a_r  # velocity magnitude
-
-#u_r = U_r*sin(theta)
-#v_r = -U_r*cos(theta)
-v_r = U_r*sin(theta)
-w_r = -U_r*cos(theta)
-println("v_r = ", v_r)
-println("w_r = ", w_r)
-u_r = 0.0
-e_r = cv*p_r/(rho_r*R)
-E_r = rho_r*e_r + 0.5*rho_r*U_r*U_r
-
-# convert velocities back to base frame of reference
-#u_r = u_r*cos(theta3)
-#w_r = u_r*sin(theta3)
-
-# save solution to sol
-sol[1] = rho_r
-sol[2] = rho_r*u_r
-sol[3] = rho_r*v_r
-sol[4] = rho_r*w_r
-sol[5] = E_r
-
-println("--- finished calcIsentropicVortex2 ---")
-return nothing
-
-end
-
-
 
 function calcIsentropicVortex{Tmsh, Tsol}(coords::AbstractArray{Tmsh}, 
                               params::ParamType{3}, sol::AbstractVector{Tsol})
 # calculates the solution at a point of the isentropic vortex
 
 
-# unpack arguments
-x = coords[1]; x_orig = x
-y = coords[2]; y_orig = y
-z = coords[3]; z_orig = z;
+  # unpack arguments
+  x = coords[1]; x_orig = x
+  y = coords[2]; y_orig = y
+  z = coords[3]; z_orig = z;
 
-#println("x = ", x, ", y = ", y, ", z = ", z)
+  #println("x = ", x, ", y = ", y, ", z = ", z)
 
-phi_z = pi/4  # angle of axis relative to z axis
-sgn = 1.0
+  phi_z = pi/4  # angle of axis relative to z axis
+  sgn = 1.0
 
-# calculate new x coordinate in rotated coordinate system
-theta1 = atan2(z, x)
-#println("theta1 = ", theta1)
-phi2 = 0.5*pi - theta1
-#println("phi2 = ", phi2)
-r_xz = sqrt(x*x + z*z)  # distance from origin to point in xz plane
-#println("r_xz = ", r_xz)
-x = r_xz*sin(phi_z + phi2)  # x coordinate in rotated system
-#println("x = ", x)
-#println("diff = ", abs(x - z_orig))
-### Debug ###
-lAB = r_xz*cos(phi_z + phi2)
-#println("lAB = ", lAB)
-theta2 = 0.5*pi - phi_z
-#println("theta2 = ", theta2)
-Bx = -lAB*cos(theta2)
-Bz = lAB*sin(theta2)
-#println("Bx = ", Bx, ", Bz = ", Bz)
-theta3_debug = atan2(z_orig - Bz, x_orig - Bx)
-### end Debug ###
+  # calculate new x coordinate in rotated coordinate system
+  theta1 = atan2(z, x)
+  #println("theta1 = ", theta1)
+  phi2 = 0.5*pi - theta1
+  #println("phi2 = ", phi2)
+  r_xz = sqrt(x*x + z*z)  # distance from origin to point in xz plane
+  #println("r_xz = ", r_xz)
+  x = r_xz*sin(phi_z + phi2)  # x coordinate in rotated system
+  #println("x = ", x)
+  #println("diff = ", abs(x - z_orig))
 
-theta3 = theta1 + phi_z + phi2 - 0.5*pi
+  theta3 = theta1 + phi_z + phi2 - 0.5*pi
 
-@assert abs(theta3_debug - theta3) < 1e-12
-#println("theta1 = ", theta1)
-#println("delta_theta = ", delta_theta)
-#println("x_orig = ", x, ", x_orig = ", x_orig)
-#println("z_orig = ", z_orig)
-#@assert abs(x - x_orig) < 1e-12
-
-# get some values out of eqn
-cv = params.cv
-R = params.R
-gamma = params.gamma
-
-# the (hard coded) parameters are
-r_in = 1  # inner radius of sector of circle
-rho_in = 2 # density at inner radius
-M_in = 0.95  # Mach number
-p_in =  1/gamma
-
-# calculate r, theta coordinates from x,y
-r = sqrt(x*x + y*y)
-if phi_z > 0
-  theta = atan2(x, y)
-#  println("atan($x, $y) = ", theta)
-else
-  theta = atan2(y, x)
-#  println("atan($y, $x) = ", theta)
-end
-@assert theta > 0.0
-@assert theta < pi/2
-# calculate values at r radius
-tmp1 = ((gamma-1)/2)*M_in*M_in
-rho_r = rho_in*(1 + tmp1*(1- (r_in*r_in)/(r*r)))^(1/(gamma-1))
-#println("rho_r = ", rho_r)
-p_r = p_in*(rho_r/rho_in)^gamma
-#println("p_r = ", p_r)
-a_r = sqrt( gamma*p_r/rho_r )
-#println("a_r = ", a_r)
-M_r = sqrt( (2/(gamma-1))*((rho_in/rho_r)^(gamma-1))*(1 + tmp1) - 2/(gamma-1) )
-#println("M_r = ", M_r)
-U_r = M_r*a_r  # velocity magnitude
-#println("U_r = ", U_r)
-if phi_z > 0
-  v_r = U_r*sin(theta)
-  u_r = -U_r*cos(theta)
-else
-  u_r = U_r*sin(theta)
-  v_r = -U_r*cos(theta)
-end
-#println("u_r = ", u_r)
-#println("v_r = ", v_r)
-#w_r = 0.0
-e_r = cv*p_r/(rho_r*R)
-E_r = rho_r*e_r + 0.5*rho_r*U_r*U_r
-
-# convert velocities back to base frame of reference
-#println("theta3 = ", theta3)
-w_r = u_r*sin(theta3)
-u_r = u_r*cos(theta3)
-#println("u_r = ", u_r)
-#println("w_r = ", w_r)
-#@assert abs(u_r) < 1e-12
-# save solution to sol
-sol[1] = rho_r
-sol[2] = rho_r*u_r
-sol[3] = rho_r*v_r
-sol[4] = rho_r*w_r
-sol[5] = E_r
-#=
-q2 = zeros(sol)
-if phi_z < pi/4
-  calcIsentropicVortex1(coords, params, q2)
-else
-  calcIsentropicVortex2(coords, params, q2)
-end
-
-println("sol = \n", real(sol))
-println("q2 = \n", real(q2))
-for i=1:5
-  println("sol[$i] = ", sol[i])
-  println("q2[$i] = ", q2[i])
-  println("diff = ", abs(sol[i] - q2[i]))
-  @assert abs(sol[i] - q2[i]) < 1e-12
-end
-=#
-return nothing
-
-end
-
-#=
-function isentropicVortexVerify(x, y, params, sol)
-  # get some values out of eqn
-  println("--- entered isentropicVortexVerify ---")
+  # get some values out of params
   cv = params.cv
   R = params.R
   gamma = params.gamma
@@ -506,38 +116,44 @@ function isentropicVortexVerify(x, y, params, sol)
 
   # calculate r, theta coordinates from x,y
   r = sqrt(x*x + y*y)
-  theta = atan2(y,x)  # angle in radians
-  println("theta = ", theta)
-  @assert theta > 0.0
+  if phi_z > 0
+    theta = atan2(x, y)
+  else
+    theta = atan2(y, x)
+  end
+
   # calculate values at r radius
   tmp1 = ((gamma-1)/2)*M_in*M_in
   rho_r = rho_in*(1 + tmp1*(1- (r_in*r_in)/(r*r)))^(1/(gamma-1))
-  println("rho_r = ", rho_r)
   p_r = p_in*(rho_r/rho_in)^gamma
-  println("p_r = ", p_r)
   a_r = sqrt( gamma*p_r/rho_r )
-  println("a_r = ", a_r)
   M_r = sqrt( (2/(gamma-1))*((rho_in/rho_r)^(gamma-1))*(1 + tmp1) - 2/(gamma-1) )
   U_r = M_r*a_r  # velocity magnitude
-  println("U_r = ", U_r)
-  u_r = 0.0
-  v_r = U_r*sin(theta)
-  w_r = U_r*cos(theta)
+
+  if phi_z > 0
+    v_r = U_r*sin(theta)
+    u_r = -U_r*cos(theta)
+  else
+    u_r = U_r*sin(theta)
+    v_r = -U_r*cos(theta)
+  end
   e_r = cv*p_r/(rho_r*R)
   E_r = rho_r*e_r + 0.5*rho_r*U_r*U_r
 
-  println("v_r = ", v_r)
-  println("w_r = ", w_r)
   # convert velocities back to base frame of reference
+  w_r = u_r*sin(theta3)
+  u_r = u_r*cos(theta3)
+
   # save solution to sol
   sol[1] = rho_r
   sol[2] = rho_r*u_r
-  sol[3] = -rho_r*v_r
-  sol[4] = -rho_r*w_r
+  sol[3] = rho_r*v_r
+  sol[4] = rho_r*w_r
   sol[5] = E_r
-  println("--- finished isentropicVortexVerify ---")
+
+  return nothing
+
 end
-=#
 
 
 @doc """
