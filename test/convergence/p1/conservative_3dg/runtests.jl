3975bd3868d39b0be680e5866fc26fe2e40a6fec
Jared Crean <jcrean01@gmail.com>
2016-07-22 15:04:08 -0400

    add 3d convergence rate tests

---
+++ test/convergence/p1/conservative_3dg/runtests.jl
@@ -0,0 +1,35 @@
+global const STARTUP_PATH = joinpath(Pkg.dir("PDESolver"), "src/solver/euler/startup.jl")
+using FactCheck
+
+#run(`cptest.sh`)
+#run(`cperr.sh`)
+facts("---- P1 Conservative DG Convergence Tests -----") do
+start_dir = pwd()
+
+resize!(ARGS, 1)
+
+cd("./m1")
+ARGS[1] = "input_vals_vortex3.jl"
+include(STARTUP_PATH)
+
+cd("../m2")
+ARGS[1] = "input_vals_vortex3.jl"
+include(STARTUP_PATH)
+
+cd("..")
+include("calc_line.jl")
+
+slope = calc_line()
+println("slope = ", slope)
+
+data = readdlm("err_data.dat")
+err_vals = data[:, 2]
+#println("err_vals = ", err_vals)
+
+slope_val = 1.84
+slope_margin = 0.1
+
+@fact slope --> greater_than(slope_val - slope_margin)
+@fact slope --> less_than(slope_val + slope_margin)
+
+end
