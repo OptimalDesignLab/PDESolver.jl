aaabf686dd33b53d1cb64f49af28f56dc4e1bc27
Anthony Ashley <ashlea@rpi.edu>
2016-12-04 15:58:55 -0500

    Fixed bug in flag/jac_method handling in initialization.jl

--- src/Utils/initialization.jl
+++ src/Utils/initialization.jl
@@ -16,7 +16,9 @@ function createMeshAndOperator(opts, dofpernode)
   order = opts["order"]  # order of accuracy
   # flag determines whether to calculate u, dR/du, or dR/dx (1, 2, or 3)
   flag = opts["run_type"]
-  jac_method = opts["jac_method"]
+  if haskey(opts, "jac_method")
+    jac_method = opts["jac_method"]
+  end
   dim = opts["dimensions"]
 
   if flag == 1 || flag == 8  || flag == 9 || flag == 10  # normal run
@@ -49,16 +51,20 @@ function createMeshAndOperator(opts, dofpernode)
     Tsbp = Float64
     Tsol = Complex128
     Tres = Complex128
-  elseif flag == 20 && jac_method == 1 # Crank-Nicolson, FD Jac
-    Tmsh = Float64
-    Tsbp = Float64
-    Tsol = Float64
-    Tres = Float64
-  elseif flag == 20 && jac_method == 2 # Crank-Nicolson, CS Jac
-    Tmsh = Float64
-    Tsbp = Float64
-    Tsol = Complex128
-    Tres = Complex128
+  elseif flag == 20 && isdefined(jac_method)
+    if jac_method == 1 # Crank-Nicolson, FD Jac
+      Tmsh = Float64
+      Tsbp = Float64
+      Tsol = Float64
+      Tres = Float64
+    elseif jac_method == 2 # Crank-Nicolson, CS Jac
+      Tmsh = Float64
+      Tsbp = Float64
+      Tsol = Complex128
+      Tres = Complex128
+    end
+  else
+    throw(ErrorException("Illegal flag or jac_method combination specified in input."))
   end
   # If the user specifies a flag other than the ones within the above if checks, 
   #   then an error will be thrown now because Tsol is not defined
