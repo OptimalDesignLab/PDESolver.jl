2be30b8b23af180b75dd28f1de912727ac40ed32
Jared Crean <jcrean01@gmail.com>
2016-12-18 20:44:34 -0500

    advection test makeover done

--- test/Advection/runtests_advection.jl
+++ test/Advection/runtests_advection.jl
@@ -66,26 +66,11 @@ facts("----- Running Advection tests -----") do
   run_testlist(AdvectionTests, tags)
 end
 
-
+#------------------------------------------------------------------------------
+# cleanup
 
 if MPI.Initialized()
   MPI.Finalize()
 end
 
 FactCheck.exitstatus()
-
-
-# write your own tests here
-# @test 1 == 1
-
-
-# using SummationByParts
-# #using Base.Test
-# using FactCheck
-# 
-# include("test_orthopoly.jl")
-# include("test_symcubatures.jl")
-# include("test_cubature.jl")
-# include("test_SummationByParts.jl")
-# 
-# FactCheck.exitstatus()
--- test/Advection/runtests_parallel4.jl
+++ test/Advection/runtests_parallel4.jl
@@ -1,3 +1,5 @@
+# run 4 processor tests
+
 push!(LOAD_PATH, joinpath(Pkg.dir("PumiInterface"), "src"))
 push!(LOAD_PATH, joinpath(Pkg.dir("PDESolver"), "src/solver/advection"))
 push!(LOAD_PATH, joinpath(Pkg.dir("PDESolver"), "src/NonlinearSolvers"))
@@ -15,8 +17,17 @@ using NonlinearSolvers   # non-linear solvers
 using ArrayViews
 
 global const STARTUP_PATH = joinpath(Pkg.dir("PDESolver"), "src/solver/advection/startup_advection.jl")
-# insert a command line argument
-resize!(ARGS, 1)
+
+#------------------------------------------------------------------------------
+# define tests and tags
+
+include("../TestSystem.jl")
+# define tags that will be used
+
+# test list
+global const AdvectionTests = TestList()
+
+
 
 function runtests_parallel4()
   facts("----- Testing Parallel 4 -----") do
@@ -30,7 +41,28 @@ function runtests_parallel4()
   return nothing
 end
 
-runtests_parallel4()
+#runtests_parallel4()
+add_func1!(AdvectionTests, runtests_parallel4)
+
+#------------------------------------------------------------------------------
+# run tests
+facts("----- Running Advection 2 processor tests -----") do
+  nargs = length(ARGS)
+  if nargs == 0
+    tags = ASCIIString[TAG_DEFAULT]
+  else
+    tags = Array(ASCIIString, nargs)
+    copy!(tags, ARGS)
+  end
+
+  resize!(ARGS, 1)
+  ARGS[1] = ""
+  run_testlist(AdvectionTests, tags)
+end
+
+#------------------------------------------------------------------------------
+# cleanup
+
 
 if MPI.Initialized()
   MPI.Finalize()
