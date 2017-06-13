2e67ea8c077c75bf3b74aa29de2127b395ba725b
Jared Crean <jcrean01@gmail.com>
2017-06-12 22:03:18 -0400

    add some debug outpt

--- test/advection/test_utils.jl
+++ test/advection/test_utils.jl
@@ -46,6 +46,7 @@ function test_area(mesh, sbp, eqn, opts)
   
   #----------------------------------------------------------------------------
   # test computeNormal
+  println("testing computeNormal")
   nout = mesh.dim*mesh.numNodesPerFace*mesh.numBoundaryFaces
   nin = mesh.dim*mesh.dim*mesh.numNodesPerFace*mesh.numBoundaryFaces
   jac = zeros(nout, nin)
@@ -90,6 +91,7 @@ i
 
   #----------------------------------------------------------------------------
   # test computeVolumeContribution
+  println("testing computeVolumecontribution")
   vol = calcVolumeContribution!(mesh, eqn, [1])
   @fact vol --> roughly(8, atol=1e-12)
 
@@ -119,7 +121,8 @@ i
 
   #----------------------------------------------------------------------------
   # test calcProjectedAreaContribution
- 
+  println("testing calcProjectedAreaContribution")
+
   proj_area = calcProjectedAreaContribution!(mesh, eqn, [1], 1)
   @fact proj_area --> roughly(4, atol=1e-12)
 
@@ -161,7 +164,7 @@ i
   @fact diffnorm --> roughly(0.0, atol=1e-5)
 
   # check the other method
-#  println("checking other method")
+  println("testing other method")
 #  nrm0 = Utils.computeNormal(mesh, eqn, mesh.bndryfaces,)
   nrm0 = copy(mesh.nrm_bndry)
   coords = mesh.coords_bndry
