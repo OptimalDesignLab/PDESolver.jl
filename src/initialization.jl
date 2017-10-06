272f69283d46e83c0018a65d6e28e3cbf2adea6a
jfyan <j4yan@rpi.edu>
2017-10-06 13:43:25 -0400

    added 3D viscous terms, but not tested

--- src/initialization.jl
+++ src/initialization.jl
@@ -1,21 +1,19 @@
 """
-  Create a SBP operator and a mesh.  This is used by all physics modules
-  to create the right type of operator and mesh based on the input options.
-  It is type unstable, but that is ok.
-
-  Inputs:
-    opts: options dictonary
-    dofpernode: number of degrees of freedom on each node
-
-  Outputs
-    sbp : an AbstractSBP
-    mesh : an AbstractMesh
-    pmesh : an AbstractMesh, used for preconditioning, may be same object as
-            mesh
-    Tsol : DataType that should be used for eqn.q
-    Tres : DataType that should be used for eqn.res
-    Tmsh : DataType of mesh.dxidx and friends
-    mesh_time : time in seconds for creation of mesh (Float64)
+Create a SBP operator and a mesh.  This is used by all physics modules
+to create the right type of operator and mesh based on the input options.
+It is type unstable, but that is ok.
+Inputs:
+  opts: options dictonary
+  dofpernode: number of degrees of freedom on each node
+Outputs
+  sbp : an AbstractSBP
+  mesh : an AbstractMesh
+  pmesh : an AbstractMesh, used for preconditioning, may be same object as
+  mesh
+  Tsol : DataType that should be used for eqn.q
+  Tres : DataType that should be used for eqn.res
+  Tmsh : DataType of mesh.dxidx and friends
+  mesh_time : time in seconds for creation of mesh (Float64)
 """
 function createMeshAndOperator(opts, dofpernode)
 
@@ -110,15 +108,29 @@ function createMeshAndOperator(opts, dofpernode)
   # should shape_type live here or be encapsulated in Pumi?
   if opts["use_DG"]
     if opts["operator_type"] == "SBPOmega"
-      reorder = false
-      internal = true
+      #      reorder = false
+      #      internal = true
+      if dim == 2
+        sbp = getTriSBPOmega(degree=order, Tsbp=Tsbp)
+      else
+        sbp = getTetSBPOmega(degree=order, Tsbp=Tsbp)
+      end
       shape_type = 2
     elseif opts["operator_type"] == "SBPGamma"
-      reorder = false
-      internal = false
+      #      reorder = false
+      #      internal = false
+      if dim == 2
+        sbp = getTriSBPGamma(degree=order, Tsbp=Tsbp)
+      else
+        sbp = getTetSBPGamma(degree=order, Tsbp=Tsbp)
+      end
       shape_type = 3
     elseif opts["operator_type"] == "SBPDiagE"
-      # nothing to do
+      if dim == 2
+        sbp = getTriSBPWithDiagE(degree=order, Tsbp=Tsbp)
+      else
+        throw(ArgumentError("3 dimensional SBPDiagonalE no supported"))
+      end
       shape_type = 4
     else
       op_type = opts["operator_type"]
@@ -130,8 +142,9 @@ function createMeshAndOperator(opts, dofpernode)
       throw(ArgumentError("invalid operator type $op_type for CG"))
     end
     # the CG varient of SBP gamma is the only option
-    reorder = true
-    internal = false
+    #    reorder = true
+    #    internal = false
+    sbp = getTriSBPGamma(degree=order, Tsbp=Tsbp)
     shape_type = 1
   end
 
@@ -140,36 +153,25 @@ function createMeshAndOperator(opts, dofpernode)
     println("\nConstructing SBP Operator")
     # create DG SBP operator with internal nodes only
     if dim == 2
-      # sbp = TriSBP{Float64}(degree=order, reorder=reorder, internal=internal)
-      if opts["operator_type"] == "SBPOmega"
-        sbp = getTriSBPOmega(degree = order)
-        sbpface = TriFace{Float64}(order, sbp.cub, sbp.vtx)
-      elseif opts["operator_type"] == "SBPGamma"
-        sbp = getTriSBPGamma(degree = order)
-        sbpface = TriFace{Float64}(order, sbp.cub, sbp.vtx)
-      elseif opts["operator_type"] == "SBPDiagE"
-        sbp = getTriSBPWithDiagE(degree = order)
-        sbpface = getTriFaceForDiagE(order, sbp.cub, sbp.vtx)
-      end
-    
+      #      sbp = TriSBP{Float64}(degree=order, reorder=reorder, internal=internal)
       # TODO: use sbp.vtx instead
       ref_verts = [-1. 1 -1; -1 -1 1]
-#      interp_op = SummationByParts.buildinterpolation(sbp, ref_verts)
-    else 
-      # sbp = TetSBP{Float64}(degree=order, reorder=reorder, internal=internal)
-      if opts["operator_type"] == "SBPOmega"
-        sbp = getTetSBPOmega(degree = order)
-      elseif opts["operator_type"] == "SBPGamma"
-        sbp = getTetSBPGamma(degree = order)
-      elseif opts["operator_type"] == "SBPDiagE"
-        throw(ArgumentError("SBPDiagE not available for 3D"))
+      #      interp_op = SummationByParts.buildinterpolation(sbp, ref_verts)
+      if opts["operator_type"] == "SBPDiagE"
+        sbpface = getTriFaceForDiagE(order, sbp.cub, ref_verts.')
+      else
+        sbpface = TriFace{Float64}(order, sbp.cub, ref_verts.')
       end
+    else
+      #      sbp = TetSBP{Float64}(degree=order, reorder=reorder, internal=internal)
       ref_verts = sbp.vtx
-#      interp_op = SummationByParts.buildinterpolation(sbp, ref_verts.')
+      #      interp_op = SummationByParts.buildinterpolation(sbp, ref_verts.')
       face_verts = SummationByParts.SymCubatures.getfacevertexindices(sbp.cub)
       topo = ElementTopology{3}(face_verts)
       sbpface = TetFace{Float64}(order, sbp.cub, ref_verts)
     end
+
+    # create mesh with 4 dof per node
 
     println("constructing DG mesh")
     if dim == 2
@@ -181,7 +183,7 @@ function createMeshAndOperator(opts, dofpernode)
     if (opts["jac_type"] == 3 || opts["jac_type"] == 4) && opts["use_jac_precond"]
       @assert dim == 2
       pmesh = PumiMeshDG2Preconditioning(mesh, sbp, opts;
-                     coloring_distance=opts["coloring_distance_prec"])
+                                         coloring_distance=opts["coloring_distance_prec"])
     else
       pmesh = mesh
     end
@@ -189,7 +191,7 @@ function createMeshAndOperator(opts, dofpernode)
   else  # continuous Galerkin
     # create SBP object
     println("\nConstructing SBP Operator")
-    sbp = TriSBP{Float64}(degree=order, reorder=reorder, internal=internal)  # create linear sbp operator
+    #    sbp = TriSBP{Float64}(degree=order, reorder=reorder, internal=internal)  # create linear sbp operator
     sbpface = TriFace{Float64}(order, sbp.cub, sbp.vtx)
     # create linear mesh with 4 dof per node
 
@@ -202,36 +204,34 @@ function createMeshAndOperator(opts, dofpernode)
       pmesh = mesh
     end
   end
+  println("numColors = ", mesh.numColors)
 
   return sbp, mesh, pmesh, Tsol, Tres, Tmsh, mesh_time
 end
 
 
 """
-  This function takes in the 4 principle object, fully initialized, and calls
-  a nonlinear solver on them, according to the options in the dictionary.
-  The evalResidual function is passed to the nonlinear solver
-
-  Inputs:
-    mesh: a mesh object
-    sbp: an SBP operator
-    eqn: an equation object
-    opts: options dictionary, used to determine which nonlinear solver to call
-    pmesh: mesh used for calculating preconditioning jacobian in Newton's
-           method, default to using mesh if not specified
-
-  Outputs:
-    none
-
-  Aliasing restrictions: none (specificaly, mesh and pmesh *can* be the same
-                         object)
+This function takes in the 4 principle object, fully initialized, and calls
+a nonlinear solver on them, according to the options in the dictionary.
+The evalResidual function is passed to the nonlinear solver
+Inputs:
+mesh: a mesh object
+sbp: an SBP operator
+eqn: an equation object
+opts: options dictionary, used to determine which nonlinear solver to call
+pmesh: mesh used for calculating preconditioning jacobian in Newton's
+method, default to using mesh if not specified
+Outputs:
+none
+Aliasing restrictions: none (specificaly, mesh and pmesh *can* be the same
+object)
 """
 function call_nlsolver(mesh::AbstractMesh, sbp::AbstractSBP,
                        eqn::AbstractSolutionData, opts::Dict,
                        pmesh::AbstractMesh=mesh)
   flag = opts["run_type"]::Int
   t = 0.0  # for steady methods, t = 0.0 always, for unsteady, the time
-           # stepper returns a new t value
+  # stepper returns a new t value
   if opts["solve"]
 
     solve_time = @elapsed if flag == 1 # normal run
@@ -240,27 +240,25 @@ function call_nlsolver(mesh::AbstractMesh, sbp::AbstractSBP,
       t_max = opts["t_max"]
 
       @time t = rk4(evalResidual, delta_t, t_max, mesh, sbp, eqn, opts, 
-                res_tol=opts["res_abstol"], real_time=opts["real_time"])
+                    res_tol=opts["res_abstol"], real_time=opts["real_time"])
       println("finish rk4")
-  #    printSolution("rk4_solution.dat", eqn.res_vec)
+      #    printSolution("rk4_solution.dat", eqn.res_vec)
 
     elseif flag == 2 # forward diff dR/du
-    #=
+      #=
       # define nested function
-      function dRdu_rk4_wrapper(u_vals::AbstractVector, res_vec::AbstractVector)
-        eqn.q_vec = u_vals
-        eqn.q_vec = res_vec
-        rk4(evalResidual, delta_t, t_max, mesh, sbp, eqn)
-        return nothing
-      end
-
-      # use ForwardDiff package to generate function that calculate jacobian
-      calcdRdu! = forwarddiff_jacobian!(dRdu_rk4_wrapper, Float64,
-                  fadtype=:dual, n = mesh.numDof, m = mesh.numDof)
-
-      jac = zeros(Float64, mesh.numDof, mesh.numDof)  # array to be populated
-      calcdRdu!(eqn.q_vec, jac)
-    =#
+        function dRdu_rk4_wrapper(u_vals::AbstractVector, res_vec::AbstractVector)
+          eqn.q_vec = u_vals
+          eqn.q_vec = res_vec
+          rk4(evalResidual, delta_t, t_max, mesh, sbp, eqn)
+          return nothing
+        end
+        # use ForwardDiff package to generate function that calculate jacobian
+          calcdRdu! = forwarddiff_jacobian!(dRdu_rk4_wrapper, Float64,
+          fadtype=:dual, n = mesh.numDof, m = mesh.numDof)
+          jac = zeros(Float64, mesh.numDof, mesh.numDof)  # array to be populated
+          calcdRdu!(eqn.q_vec, jac)
+          =#
     elseif flag == 3 # calculate dRdx
 
       # dRdx here
@@ -280,9 +278,9 @@ function call_nlsolver(mesh::AbstractMesh, sbp::AbstractSBP,
       end
 
       function post_func(mesh, sbp, eqn, opts)
-  #      for i=1:mesh.numDof
-  #        eqn.res_vec[i] *= eqn.Minv[i]
-  #      end
+        #      for i=1:mesh.numDof
+        #        eqn.res_vec[i] *= eqn.Minv[i]
+        #      end
         nrm = norm(eqn.res_vec)
         println("post_func returning residual norm = ", nrm)
         return nrm
@@ -306,8 +304,8 @@ function call_nlsolver(mesh::AbstractMesh, sbp::AbstractSBP,
 
 
       t = rk4(evalResidual, delta_t, t_max, eqn.q_vec, eqn.res_vec, test_pre_func,
-          test_post_func, (mesh, sbp, eqn), opts, 
-          majorIterationCallback=eqn.majorIterationCallback, real_time=opts["real_time"])
+              test_post_func, (mesh, sbp, eqn), opts, 
+              majorIterationCallback=eqn.majorIterationCallback, real_time=opts["real_time"])
 
 
     elseif flag == 20
@@ -327,14 +325,14 @@ function call_nlsolver(mesh::AbstractMesh, sbp::AbstractSBP,
 
     elseif flag == 41  # special mode: use regular Newton to solve homotopy
 
-     @time newton(evalHomotopy, mesh, sbp, eqn, opts, pmesh, itermax=opts["itermax"],
+      @time newton(evalHomotopy, mesh, sbp, eqn, opts, pmesh, itermax=opts["itermax"],
                    step_tol=opts["step_tol"], res_abstol=opts["res_abstol"],
                    res_reltol=opts["res_reltol"], res_reltol0=opts["res_reltol0"])
 
 
 
     else
-       throw(ErrorException("No flag specified: no solve will take place"))
+      throw(ErrorException("No flag specified: no solve will take place"))
 
     end       # end of if/elseif blocks checking flag
 
@@ -372,18 +370,18 @@ function call_nlsolver(mesh::AbstractMesh, sbp::AbstractSBP,
     end
 
     myrank = mesh.myrank
-  #  f = open("profile_$myrank.dat", "a+")
-  #  Profile.print(f, format=:flat, C=true)
-  #  close(f)
+    #  f = open("profile_$myrank.dat", "a+")
+    #  Profile.print(f, format=:flat, C=true)
+    #  close(f)
 
     saveSolutionToMesh(mesh, real(eqn.q_vec))
-  #  printSolution(mesh, real(eqn.q_vec))
-  #  printCoordinates(mesh)
+    #  printSolution(mesh, real(eqn.q_vec))
+    #  printCoordinates(mesh)
     writeVisFiles(mesh, "solution_done")
 
     # write timings
-  #  timings = [params.t_volume, params.t_face, params.t_source, params.t_sharedface, params.t_bndry, params.t_send, params.t_wait, params.t_allreduce, params.t_jacobian, params.t_solve, params.t_barrier, params.t_barrier2, params.t_barrier3]
-  #  writedlm("timing_breakdown_$myrank.dat", vcat(timings, params.t_barriers))
+    #  timings = [params.t_volume, params.t_face, params.t_source, params.t_sharedface, params.t_bndry, params.t_send, params.t_wait, params.t_allreduce, params.t_jacobian, params.t_solve, params.t_barrier, params.t_barrier2, params.t_barrier3]
+    #  writedlm("timing_breakdown_$myrank.dat", vcat(timings, params.t_barriers))
   end  # end if (opts[solve])
 
   return nothing
