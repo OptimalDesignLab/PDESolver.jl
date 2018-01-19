6e5773f64d0862fd6be6c39bb68a032ca5763c72
Jared Crean <jcrean01@gmail.com>
2018-01-19 12:56:13 -0500

    add build matrix

---
+++ test/runtests_travis.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# this script is used to run the tests on Travis CI.
+# In particular, it is used to run different tests as part of the Travis
+# build matrix
+
+err=0
+# NOTE: these if statements must exactly match the `env` section in .travis.yml
+if [[ $TEST_ADVECTION == "1" ]];
+then
+  cd ./advection
+  julia ./runtests.jl tag_shorttest
+  err=$(( err + $?))
+
+  mpirun -np 2 julia ./runtests_parallel.jl tag_shorttest
+  err=$(( err + $?))
+
+  mpirun -np 4 julia ./runtests_parallel.jl tag_shorttest
+  err=$(( err + $?))
+
+  cd ..
+fi
+
+if [[ $TEST_EULER == "1" ]];
+then
+  cd ./euler
+  julia ./runtests.jl tag_shorttest
+  err=$(( err + $?))
+
+  mpirun -np 2 julia ./runtests_parallel.jl tag_shorttest
+  err=$(( err + $?))
+
+  mpirun -np 4 julia ./runtests_parallel.jl tag_shorttest
+  err=$(( err + $?))
+
+  cd ..
+fi
+
+if [[ $TEST_SIMPLEODE == "1" ]];
+then
+  cd ./simpleODE
+  julia ./runtests.jl tag_shorttest
+  err=$(( err + $?))
+
+  cd ..
+fi
+
+
+
+exit $err
