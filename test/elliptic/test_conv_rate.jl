32fe32979f383c3f697fbb3d372216e1d65ffc60
Jared Crean <jcrean01@gmail.com>
2018-06-01 16:28:30 -0400

    loosen test tolerances

--- test/elliptic/test_conv_rate.jl
+++ test/elliptic/test_conv_rate.jl
@@ -6,8 +6,8 @@ function test_elliptic_conv_rate()
         func0  = 6.435906901469668e-5
         l2nrm = readdlm("l2norm.dat") 
         func  = readdlm("functional.dat") 
-        @fact l2nrm[1] --> roughly(l2nrm0, atol=1e-13)
-        @fact func[1]  --> roughly(func0,  atol=1e-13)
+        @fact l2nrm[1] --> roughly(l2nrm0, atol=1e-10)
+        @fact func[1]  --> roughly(func0,  atol=1e-10)
 
         input_file = "./input_conv_study2.jl"
         mesh, sbp, eqn, opts = solvePDE(input_file)
@@ -15,8 +15,8 @@ function test_elliptic_conv_rate()
         func0  = 1.6477731494431852e-5
         l2nrm = readdlm("l2norm.dat") 
         func  = readdlm("functional.dat") 
-        @fact l2nrm[1] --> roughly(l2nrm0, atol=1e-13)
-        @fact func[1]  --> roughly(func0,  atol=1e-13)
+        @fact l2nrm[1] --> roughly(l2nrm0, atol=1e-10)
+        @fact func[1]  --> roughly(func0,  atol=1e-10)
 
         input_file = "./input_conv_study3.jl"
         mesh, sbp, eqn, opts = solvePDE(input_file)
@@ -26,8 +26,8 @@ function test_elliptic_conv_rate()
         func  = readdlm("functional.dat") 
         println(l2nrm)
         println(func)
-        @fact l2nrm[1] --> roughly(l2nrm0, atol=1e-12)
-        @fact func[1]  --> roughly(func0,  atol=1e-12)
+        @fact l2nrm[1] --> roughly(l2nrm0, atol=1e-10)
+        @fact func[1]  --> roughly(func0,  atol=1e-10)
     end
 end
 
