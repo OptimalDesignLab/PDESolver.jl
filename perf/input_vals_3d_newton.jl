fd11c914478e0ea2cf524b88c2038db19f77f091
Jared Crean <jcrean01@gmail.com>
2018-06-04 15:32:29 -0400

    add 3d newton test

---
+++ perf/input_vals_3d_newton.jl
@@ -0,0 +1,32 @@
+# 3D Newton's method: run for specified number of iterations
+
+
+arg_dict = Dict{Any, Any}(
+"physics" => "Euler",
+"run_type" => 5,
+"jac_type" => 2,
+"jac_method" => 2,
+"order" => 1,
+"use_DG" => true,
+"Flux_name" => "RoeFlux",
+"IC_name" => "ICExp",
+"SRCname" => "SRCExp",
+"dimensions" => 3,
+"numBC" => 1,
+"BC1" => [ 0, 1, 2, 3, 4, 5],
+"BC1_name" => "ExpBC",
+"delta_t" => 0.00005,
+"t_max" => 500.000,
+"smb_name" => "SRCMESHES/cube_benchmarksmall.smb",
+"dmg_name" => ".null",
+"res_abstol" => -1e-8,
+"res_reltol" => -1e-20,
+"step_tol" => 1e-10,
+"itermax" => 100,
+"output_freq" => 100,
+"step_tol" => -1.0,
+"do_postproc" => true,
+"exact_soln_func" => "ICExp",
+"write_timing" => true,
+"solve" => true,
+)
