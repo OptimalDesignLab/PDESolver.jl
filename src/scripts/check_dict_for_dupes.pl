879c64df7a5c379315f67be675eeae7743cb28f2
Anthony Ashley <ashlea@rpi.edu>
2018-08-14 14:21:13 -0400

    added dupe check support for multiple key-vals on 1 line. added option to disable. added list of possible duplicates to output.

--- src/scripts/check_dict_for_dupes.pl
+++ src/scripts/check_dict_for_dupes.pl
@@ -7,12 +7,14 @@
 
 my $counter = 0;      # This counter should correspond to the number of input options 
                       #   specified in the input dictionary.
+
 my %hash_for_check;   # We want to assign the dict key-value pairs from the file
                       #   to this hash. Perl hashes act like Julia dicts, so duplicates get
                       #   overwritten. We will compare these two lengths.
 
+my %duplicates;       # Hash for storing duplicates
+
 while (<>) {          # while there are lines of stdin (loop over file lines)
-  no warnings;
   if (/Dict{Any,Any}/) {    # skip the first line
     next;
   }
@@ -26,7 +28,6 @@ while (<>) {          # while there are lines of stdin (loop over file lines)
     next;
   }
   # match:
-  #   beginning of line
   #   possible whitespace
   #   quotation mark
   #   at least one character that isn't a quotation mark    -> stored in $1
@@ -34,26 +35,30 @@ while (<>) {          # while there are lines of stdin (loop over file lines)
   #   possible whitespace
   #   the string "=>"
   #   possible whitespace
-  #   at least one character, unspecified type    -> stored in $2
+  #   at least one character that isn't a comma    -> stored in $2
   #   possible whitespace
   #   comma
-  #   possible whitespace
-  #   end of line
-  # The intent here is to match exactly the key & value pair of each line in the dictionary file.
-  # This is purposefully fairly stringent.
-  if (/^\s*\"([^"]+)\"\s*=>\s*(.+)\s*,\s*$/) {      
+  # The intent here is to match exactly the key & value pair in the dictionary file.
+  # It should match multiple key-value pairs per line.
+  while (/\"([^"]+)\"\s*=>\s*([^,]+)\s*,/g) {
+    $counter++;
+    if (exists $hash_for_check{$1}) {     # check if key already exists, if so, add to the duplicates hash
+      $duplicates{$1} = $2;
+    }
     # build a perl hash of all the key-value pairs (similar to julia Dict)
     $hash_for_check{$1} = $2;
   }
-  $counter++;
 }
 
 # If the hash length is less than the number of key-value pairs,
 #   then there probably is a duplicate.
 #   This is because perl hashes and julia dicts are built in a similar fashion:
 #   if a key already exists, it is just overwritten if it is assigned again.
-if (length(%hash_for_check) < $counter) {
-  print "=================================================\n";
-  print "  Input dictionary: possible duplicate spotted.\n";
-  print "=================================================\n";
+if (scalar keys %hash_for_check < $counter) {     # scalar gives the length of array 'keys %hash_for_check'
+  print "==================================================\n";
+  print "  Input dictionary- possible duplicates spotted:\n";
+  foreach $duplicate (keys %duplicates) {
+    print "    ", $duplicate, "\n";
+  }
+  print "==================================================\n";
 }
