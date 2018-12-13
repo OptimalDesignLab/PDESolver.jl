1e87977e65db65810c7e36416c955fc011be2bf8
Jared Crean <jcrean01@gmail.com>
2018-12-13 16:01:57 -0500

    back to ODLCommonTools master branch

--- deps/build.jl
+++ deps/build.jl
@@ -44,7 +44,7 @@ function installPDESolver()
   std_pkgs = [
               "PkgFix" "https://github.com/OptimalDesignLab/PkgFix.jl.git" "upgrade_0.6";
               "ArrayViews"   "https://github.com/JaredCrean2/ArrayViews.jl.git" "work"
-              "ODLCommonTools" "https://github.com/OptimalDesignLab/ODLCommonTools.jl.git" "v0.5";
+              "ODLCommonTools" "https://github.com/OptimalDesignLab/ODLCommonTools.jl.git" "master";
               "SummationByParts" "https://github.com/OptimalDesignLab/SummationByParts.jl.git" "jc_v0.4";
               "PumiInterface" "https://github.com/OptimalDesignLab/PumiInterface.jl.git" "v0.9";
               "PETSc2" "https://github.com/OptimalDesignLab/PETSc2.jl.git" "v0.3"
