540384c70053fc0bbca4a1d0027d1a850b79650f
Jared Crean <jcrean01@gmail.com>
2018-12-12 19:05:36 -0500

    more fixing tags

--- deps/build.jl
+++ deps/build.jl
@@ -45,7 +45,7 @@ function installPDESolver()
               "PkgFix" "https://github.com/OptimalDesignLab/PkgFix.jl.git" "upgrade_0.6";
               "ArrayViews"   "https://github.com/JaredCrean2/ArrayViews.jl.git" "work"
               "ODLCommonTools" "https://github.com/OptimalDesignLab/ODLCommonTools.jl.git" "v0.5";
-              "SummationByParts" "https://github.com/OptimalDesignLab/SummationByParts.jl.git" "jac_v0.4";
+              "SummationByParts" "https://github.com/OptimalDesignLab/SummationByParts.jl.git" "jc_v0.4";
               "PumiInterface" "https://github.com/OptimalDesignLab/PumiInterface.jl.git" "v0.9";
               "PETSc2" "https://github.com/OptimalDesignLab/PETSc2.jl.git" "v0.3"
               ]
